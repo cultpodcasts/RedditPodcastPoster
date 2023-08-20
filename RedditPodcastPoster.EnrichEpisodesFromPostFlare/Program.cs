bb5fec7e371a9cf07d3237d48060e1abc50459dd
Jon Breen <jonbreen@outlook.com>
2023-08-20 18:26:38 +0100

    Work in progress

---
+++ RedditPodcastPoster.EnrichEpisodesFromPostFlare/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RedditPodcastPoster.Common;
+using RedditPodcastPoster.Common.Persistence;
+using RedditPodcastPoster.Common.Reddit;
+using RedditPodcastPoster.EnrichEpisodesFromPostFlare;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", true)
+    .AddEnvironmentVariables("RedditPodcastPoster_")
+    .AddCommandLine(args)
+    .AddSecrets(Assembly.GetExecutingAssembly());
+
+builder.Services
+    .AddLogging()
+    .AddScoped<IFileRepository, FileRepository>()
+    .AddScoped<ICosmosDbRepository, CosmosDbRepository>()
+    .AddSingleton(new JsonSerializerOptions
+    {
+        WriteIndented = true
+    })
+    .AddScoped<SubredditPostFlareEnricher>()
+    .AddScoped<IFilenameSelector, FilenameSelector>()
+    .AddScoped<ICosmosDbKeySelector, CosmosDbKeySelector>();
+
+CosmosDbClientFactory.AddCosmosClient(builder.Services);
+RedditClientFactory.AddRedditClient(builder.Services);
+
+
+builder.Services
+    .AddOptions<CosmosDbSettings>().Bind(builder.Configuration.GetSection("cosmosdb"));
+builder.Services
+    .AddOptions<RedditSettings>().Bind(builder.Configuration.GetSection("reddit"));
+builder.Services
+    .AddOptions<SubredditSettings>().Bind(builder.Configuration.GetSection("subreddit"));
+
+
+using var host = builder.Build();
+var processor = host.Services.GetService<SubredditPostFlareEnricher>();
+await processor.Run();
