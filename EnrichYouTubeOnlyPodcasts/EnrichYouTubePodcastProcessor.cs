61e49eef6ec30aa59d1b70552f4ec22affabf3c5
Jon Breen <jonbreen@outlook.com>
2023-08-27 22:02:59 +0100

    Clean up

--- EnrichYouTubeOnlyPodcasts/EnrichYouTubePodcastProcessor.cs
+++ EnrichYouTubeOnlyPodcasts/EnrichYouTubePodcastProcessor.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.RegularExpressions;
-using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
 using Microsoft.Extensions.Logging;
 using RedditPodcastPoster.Common.Podcasts;
