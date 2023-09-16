2763c9f4e344dcc8b0481a6401c4c94cc639313b
Jon Breen <jonbreen@outlook.com>
2023-09-16 21:20:49 +0100

    Async entry point

--- Indexer/Indexer.cs
+++ Indexer/Indexer.cs
@@ -1,37 +1,24 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using RedditPodcastPoster.Common.Podcasts;
 
 namespace Indexer;
 
 public class Indexer
 {
+    private readonly IPodcastsUpdater _podcastsUpdater;
     private readonly ILogger _logger;
 
-    public Indexer(ILoggerFactory loggerFactory)
+    public Indexer(IPodcastsUpdater podcastsUpdater, ILogger<Indexer> logger)
     {
-        _logger = loggerFactory.CreateLogger<Indexer>();
+        _podcastsUpdater = podcastsUpdater;
+        _logger = logger;
     }
 
     [Function("Indexer")]
-    public void Run([TimerTrigger("0 */5 * * * *")] MyInfo myTimer)
+    public async Task Run([TimerTrigger("0 */4 * * *")] TimerInfo timerTimer)
     {
-        _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-        _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+        _logger.LogInformation($"{nameof(Run)} - C# Timer trigger function executed at: {DateTime.Now}");
+        _logger.LogInformation($"{nameof(Run)} - Next timer schedule at: {timerTimer.ScheduleStatus.Next}");
     }
-}
-
-public class MyInfo
-{
-    public MyScheduleStatus ScheduleStatus { get; set; } 
-
-    public bool IsPastDue { get; set; }
-}
-
-public class MyScheduleStatus
-{
-    public DateTime Last { get; set; }
-
-    public DateTime Next { get; set; }
-
-    public DateTime LastUpdated { get; set; }
 }
