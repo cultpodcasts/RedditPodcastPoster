7f75f64623be923ca3c7cfca57ce214cd6f6fb7a
Jon Breen <jonbreen@outlook.com>
2023-09-18 23:08:05 +0100

    Remove swagger

--- API/Program.cs
+++ API/Program.cs
@@ -1,42 +1,30 @@
 using API.Data;
 using RedditPodcastPoster.Common.Persistence;
-using System.Reflection;
-using RedditPodcastPoster.Common;
 using RedditPodcastPoster.Common.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Configuration
-//    .AddJsonFile("appsettings.json", false)
-//    .AddEnvironmentVariables("RedditPodcastPoster_")
-//    .AddCommandLine(args)
-//    .AddSecrets(Assembly.GetExecutingAssembly());
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
-
-builder.Services.AddScoped<IQueryExecutor, QueryExecutor>();
-builder.Services.AddScoped<ITextSanitiser, TextSanitiser>();
-
-CosmosDbClientFactory.AddCosmosClient(builder.Services);
 builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"])
+    .AddScoped<IQueryExecutor, QueryExecutor>()
+    .AddScoped<ITextSanitiser, TextSanitiser>()
     .AddOptions<CosmosDbSettings>().Bind(builder.Configuration.GetSection("cosmosdb"));
 
-
+CosmosDbClientFactory.AddCosmosClient(builder.Services);
 
 builder.Services.AddControllers();
 
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
 
 app.UseHttpsRedirection();
 
