ccd621299e60801cabe0f163f61201a8fa686410
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2023-09-18 18:57:45 +0100

    Feature/update single podcast from podcast services (#55)
    * Added new tool to update a single podcast
    
    * FIxed broken build
    
    * Added updated sln
    
    ---------
    
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

---
+++ EnrichSinglePodcastFromPodcastServices/EnrichSinglePodcastFromPodcastServicesProcessor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using RedditPodcastPoster.Common;
+using RedditPodcastPoster.Common.Podcasts;
+
+namespace EnrichSinglePodcastFromPodcastServices;
+
+public class EnrichSinglePodcastFromPodcastServicesProcessor
+{
+    private readonly IPodcastUpdater _podcastUpdater;
+    private readonly IPodcastRepository _podcastRepository;
+    private readonly ILogger<EnrichSinglePodcastFromPodcastServicesProcessor> _logger;
+
+    public EnrichSinglePodcastFromPodcastServicesProcessor(
+        IPodcastUpdater podcastUpdater,
+        IPodcastRepository podcastRepository,
+        ILogger<EnrichSinglePodcastFromPodcastServicesProcessor> logger)
+    {
+        _podcastUpdater = podcastUpdater;
+        _podcastRepository = podcastRepository;
+        _logger = logger;
+    }
+
+    public async Task Run(string podcastId)
+    {
+        var podcast = await _podcastRepository.GetPodcast(podcastId.ToString());
+        if (podcast == null)
+        {
+            throw new ArgumentException($"No podcast with Guid '{podcast}' found");
+        }
+
+        await _podcastUpdater.Update(podcast, new IndexOptions(null, false));
+        await _podcastRepository.Update(podcast);
+    }
+}
---
+++ EnrichSinglePodcastFromPodcastServices/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text.Json;
+using EnrichSinglePodcastFromPodcastServices;
+using iTunesSearch.Library;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RedditPodcastPoster.Common;
+using RedditPodcastPoster.Common.Episodes;
+using RedditPodcastPoster.Common.Persistence;
+using RedditPodcastPoster.Common.Podcasts;
+using RedditPodcastPoster.Common.PodcastServices;
+using RedditPodcastPoster.Common.PodcastServices.Apple;
+using RedditPodcastPoster.Common.PodcastServices.Spotify;
+using RedditPodcastPoster.Common.PodcastServices.YouTube;
+using PodcastFactory = RedditPodcastPoster.Common.Podcasts.PodcastFactory;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", true)
+    .AddEnvironmentVariables("RedditPodcastPoster_")
+    .AddCommandLine(args)
+    .AddSecrets(Assembly.GetExecutingAssembly());
+
+builder.Services
+    .AddLogging()
+    .AddSingleton(new JsonSerializerOptions
+    {
+        WriteIndented = true
+    })
+    .AddScoped<EnrichSinglePodcastFromPodcastServicesProcessor>()
+    .AddScoped<IPodcastRepository, PodcastRepository>()
+    .AddScoped<IDataRepository, CosmosDbRepository>()
+    .AddSingleton<ICosmosDbKeySelector, CosmosDbKeySelector>()
+    .AddSingleton<PodcastFactory>()
+    .AddScoped<IApplePodcastEnricher, ApplePodcastEnricher>()
+    .AddScoped<IApplePodcastResolver, ApplePodcastResolver>()
+    .AddScoped(s => new iTunesSearchManager())
+    .AddScoped<IRemoteClient, RemoteClient>()
+    .AddScoped<IApplePodcastEnricher, ApplePodcastEnricher>()
+    .AddScoped<IPodcastUpdater, PodcastUpdater>()
+    .AddScoped<IEpisodeProvider, EpisodeProvider>()
+    .AddScoped<ISpotifyEpisodeProvider, SpotifyEpisodeProvider>()
+    .AddScoped<ISpotifyItemResolver, SpotifyItemResolver>()
+    .AddScoped<ISpotifySearcher, SpotifySearcher>()
+    .AddScoped<IYouTubeEpisodeProvider, YouTubeEpisodeProvider>()
+    .AddScoped<IYouTubeSearchService, YouTubeSearchService>()
+    .AddScoped<IYouTubeItemResolver, YouTubeItemResolver>()
+    .AddScoped<IYouTubeSearcher, YouTubeSearcher>()
+    .AddScoped<IPodcastServicesEpisodeEnricher, PodcastServicesEpisodeEnricher>()
+    .AddScoped<IAppleEpisodeResolver, AppleEpisodeResolver>()
+    .AddScoped<IApplePodcastService, ApplePodcastService>()
+    .AddScoped<ICachedApplePodcastService, CachedApplePodcastService>()
+    .AddSingleton<IAppleBearerTokenProvider, AppleBearerTokenProvider>()
+    .AddHttpClient<IApplePodcastService, ApplePodcastService>((services, httpClient) =>
+    {
+        var appleBearerTokenProvider = services.GetService<IAppleBearerTokenProvider>();
+        httpClient.BaseAddress = new Uri("https://amp-api.podcasts.apple.com/");
+        httpClient.DefaultRequestHeaders.Authorization = appleBearerTokenProvider!.GetHeader().GetAwaiter().GetResult();
+        httpClient.DefaultRequestHeaders.Accept.Clear();
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
+        httpClient.DefaultRequestHeaders.Referrer = new Uri("https://podcasts.apple.com/");
+        httpClient.DefaultRequestHeaders.Add("Origin", "https://podcasts.apple.com");
+        httpClient.DefaultRequestHeaders.UserAgent.Clear();
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0");
+    });
+
+CosmosDbClientFactory.AddCosmosClient(builder.Services);
+SpotifyClientFactory.AddSpotifyClient(builder.Services);
+YouTubeServiceFactory.AddYouTubeService(builder.Services);
+
+builder.Services
+    .AddOptions<CosmosDbSettings>().Bind(builder.Configuration.GetSection("cosmosdb"));
+builder.Services
+    .AddOptions<SpotifySettings>().Bind(builder.Configuration.GetSection("spotify"));
+builder.Services
+    .AddOptions<YouTubeSettings>().Bind(builder.Configuration.GetSection("youtube"));
+
+using var host = builder.Build();
+
+
+var podcastProcessor = host.Services.GetService<EnrichSinglePodcastFromPodcastServicesProcessor>()!;
+await podcastProcessor.Run(args[0]);
+return 0;
