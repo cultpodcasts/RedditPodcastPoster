67c935937beb28c07d23d04dfa1b0f7e51f92537
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2023-09-30 10:59:42 +0100

    Reinstated reduction (#76)
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- RedditPodcastPoster.Common/PodcastServices/Spotify/SpotifyEpisodeProvider.cs
+++ RedditPodcastPoster.Common/PodcastServices/Spotify/SpotifyEpisodeProvider.cs
@@ -24,6 +24,11 @@ public class SpotifyEpisodeProvider : ISpotifyEpisodeProvider
             await _spotifyItemResolver.GetEpisodes(
                 new GetSpotifyPodcastEpisodesRequest(request.SpotifyId, request.ProcessRequestReleasedSince));
 
+        if (request.ProcessRequestReleasedSince.HasValue)
+        {
+            episodes = episodes.Where(x => x.GetReleaseDate() > request.ProcessRequestReleasedSince.Value);
+        }
+
         return episodes.Select(x =>
             Episode.FromSpotify(
                 x.Id,
