9489d7edbbeff6d47a739a07880ddd65b484d680
Jon Breen <jonbreen@outlook.com>
2023-10-08 18:37:39 +0100

    Fixed bug where podcast episodes were expected to be returned in reverse date order.

--- RedditPodcastPoster.Common/PodcastServices/Spotify/ISpotifyItemResolver.cs
+++ RedditPodcastPoster.Common/PodcastServices/Spotify/ISpotifyItemResolver.cs
@@ -6,5 +6,5 @@ public interface ISpotifyItemResolver
 {
     Task<FullEpisode?> FindEpisode(FindSpotifyEpisodeRequest request, IndexingContext indexingContext);
     Task<SpotifyPodcastWrapper?> FindPodcast(FindSpotifyPodcastRequest request, IndexingContext indexingContext);
-    Task<IEnumerable<SimpleEpisode>?> GetEpisodes(SpotifyPodcastId request, IndexingContext indexingContext);
+    Task<IEnumerable<SimpleEpisode>> GetEpisodes(SpotifyPodcastId request, IndexingContext indexingContext);
 }
