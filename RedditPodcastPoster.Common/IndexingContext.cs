b8372f86735bac83d0db9ade3fe3fb4679662579
Jon Breen <jonbreen@outlook.com>
2023-10-14 20:34:00 +0100

    Default expensive-queries to off

--- RedditPodcastPoster.Common/IndexingContext.cs
+++ RedditPodcastPoster.Common/IndexingContext.cs
@@ -4,7 +4,7 @@ public record IndexingContext(
     DateTime? ReleasedSince = null,
     bool SkipYouTubeUrlResolving = false,
     bool SkipSpotifyUrlResolving = false,
-    bool SkipExpensiveQueries = false)
+    bool SkipExpensiveQueries = true)
 {
     public bool SkipYouTubeUrlResolving { get; set; } = SkipYouTubeUrlResolving;
     public bool SkipSpotifyUrlResolving { get; set; } = SkipSpotifyUrlResolving;
