ed2a598f81d7c6085193626299e5e88a83e96471
Jon Breen <jonbreen@outlook.com>
2023-10-15 05:13:57 +0100

    Moved out lower-case handling into own class

---
+++ RedditPodcastPoster.Common/Text/LowerCaseTerms.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+using RedditPodcastPoster.Common.Extensions;
+
+namespace RedditPodcastPoster.Common.Text;
+
+public static class LowerCaseTerms
+{
+    private static readonly string[] LesserWords =
+    {
+        "the", "of", "on", "in", "to", "a", "an", "it", "not", "your", "you", "was", "isn't", "is", "want", "wants",
+        "her", "his", "from", "their", "they", "out", "come", "coming", "away", "by", "what", "who", "made", "make",
+        "since", "for", "go", "gone", "give", "gives", "given", "next", "with", "about", "how", "here", "called",
+        "call", "doing", "do", "does"
+    };
+
+    private static readonly string[] AlwaysLowerCaseWords =
+    {
+        "etc"
+    };
+
+    private static readonly string[] Ordinals =
+    {
+        "th", "st", "rd"
+    };
+
+    public static readonly IDictionary<string, Regex>
+        Expressions = LesserWords.ToDictionary(x => x,
+                x => new Regex($@"(?<!^'?""?){x}\b", RegexOptions.Compiled | RegexOptions.IgnoreCase))
+            .AddRange(AlwaysLowerCaseWords.ToDictionary(x => x,
+                x => new Regex($@"\b{x}\b", RegexOptions.Compiled | RegexOptions.IgnoreCase)))
+            .AddRange(Ordinals.ToDictionary(x => x,
+                x => new Regex($@"(?<=\d){x}", RegexOptions.Compiled | RegexOptions.IgnoreCase)));
+}
