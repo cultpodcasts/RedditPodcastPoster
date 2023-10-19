75436a3f91d09c58370d38a89800880f3ce740e1
Jon Breen <jonbreen@outlook.com>
2023-10-19 01:10:06 +0100

    Clean up ioc of submit-url

---
+++ Class-Libraries/RedditPodcastPoster.UrlSubmission/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using RedditPodcastPoster.PodcastServices.Apple;
+using RedditPodcastPoster.PodcastServices.Spotify;
+using RedditPodcastPoster.PodcastServices.YouTube;
+using RedditPodcastPoster.UrlSubmission.Categorisation;
+
+namespace RedditPodcastPoster.UrlSubmission.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddUrlSubmission(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<IUrlCategoriser, UrlCategoriser>()
+                .AddScoped<IAppleUrlCategoriser, AppleUrlCategoriser>()
+                .AddScoped<ISpotifyUrlCategoriser, SpotifyUrlCategoriser>()
+                .AddScoped<IYouTubeUrlCategoriser, YouTubeUrlCategoriser>()
+                .AddScoped<IUrlSubmitter, UrlSubmitter>();
+        }
+    }
+}
