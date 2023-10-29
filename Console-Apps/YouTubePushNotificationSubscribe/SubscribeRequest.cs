e354fdfecd3d525ca37ec47e31bbd4a31b6c740f
Jon Breen <jonbreen@outlook.com>
2023-10-29 10:35:12 +0000

    Cleaned up request

--- Console-Apps/YouTubePushNotificationSubscribe/SubscribeRequest.cs
+++ Console-Apps/YouTubePushNotificationSubscribe/SubscribeRequest.cs
@@ -1,5 +1,4 @@
 ﻿using CommandLine;
-using System;
 
 namespace YouTubePushNotificationSubcribe;
 
@@ -8,7 +7,7 @@ public class SubscribeRequest
     [Option('p', "podcast-id", Required = false, HelpText = "The Id of the podcast to enable push-notifications")]
     public Guid? PodcastId { get; set; }
 
-    [Option('u', "unsubscribe-podcast-id", Required = false, HelpText = "The Id of the podcast to disable push-notifications")]
+    [Option('u', "unsubscribe-podcast-id", Required = false,
+        HelpText = "The Id of the podcast to disable push-notifications")]
     public Guid? UnsubscribePodcastId { get; set; }
-
 }
