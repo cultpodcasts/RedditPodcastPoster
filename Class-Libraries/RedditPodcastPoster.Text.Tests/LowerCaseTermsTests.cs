6357b052c51c89a3633d5038e8100108d9430b25
Jon Breen <jonbreen@outlook.com>
2023-10-31 12:34:39 +0000

    Updated tweet console-app

---
+++ Class-Libraries/RedditPodcastPoster.Text.Tests/LowerCaseTermsTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace RedditPodcastPoster.Text.Tests
+{
+    public class LowerCaseTermsTests
+    {
+        [Fact]
+        public void Expressions_WhenEvaluated_IsCorrect()
+        {
+            // arrange
+            // act
+            Action act = () =>
+            {
+                var x = LowerCaseTerms.Expressions;
+            };
+            // assert
+            act.Should().NotThrow();
+        }
+    }
+}
