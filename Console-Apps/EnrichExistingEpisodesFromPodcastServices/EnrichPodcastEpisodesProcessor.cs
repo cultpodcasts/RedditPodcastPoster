f027dd2bb63bfaa2ef5ee0761e457bb84e1bae05
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2023-11-04 16:49:27 +0000

    Added console-app to enrich existing episodes (#166)
    * Added console-app to enrich existing episodes
    
    * Fixed non population of spotify id
    
    ---------
    
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

---
+++ Console-Apps/EnrichExistingEpisodesFromPodcastServices/EnrichPodcastEpisodesProcessor.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Extensions.Logging;
+using RedditPodcastPoster.Common;
+using RedditPodcastPoster.Models;
+using RedditPodcastPoster.Persistence.Abstractions;
+using RedditPodcastPoster.PodcastServices.Abstractions;
+using RedditPodcastPoster.PodcastServices.Apple;
+using RedditPodcastPoster.PodcastServices.Spotify;
+using RedditPodcastPoster.PodcastServices.YouTube;
+
+namespace EnrichExistingEpisodesFromPodcastServices;
+
+public class EnrichPodcastEpisodesProcessor
+{
+    private readonly IAppleUrlCategoriser _appleUrlCategoriser;
+    private readonly ILogger<EnrichPodcastEpisodesProcessor> _logger;
+    private readonly IPodcastRepository _podcastsRepository;
+    private readonly ISpotifyUrlCategoriser _spotifyUrlCategoriser;
+    private readonly IYouTubeUrlCategoriser _youTubeUrlCategoriser;
+
+    public EnrichPodcastEpisodesProcessor(
+        IPodcastRepository podcastsRepository,
+        ISpotifyUrlCategoriser spotifyUrlCategoriser,
+        IAppleUrlCategoriser appleUrlCategoriser,
+        IYouTubeUrlCategoriser youTubeUrlCategoriser,
+        ILogger<EnrichPodcastEpisodesProcessor> logger)
+    {
+        _podcastsRepository = podcastsRepository;
+        _spotifyUrlCategoriser = spotifyUrlCategoriser;
+        _appleUrlCategoriser = appleUrlCategoriser;
+        _youTubeUrlCategoriser = youTubeUrlCategoriser;
+        _logger = logger;
+    }
+
+    public async Task Run(EnrichPodcastEpisodesRequest request)
+    {
+        IndexingContext indexingContext;
+        if (request.ReleasedSince.HasValue)
+        {
+            indexingContext = new IndexingContext(DateTimeHelper.DaysAgo(request.ReleasedSince.Value));
+        }
+        else
+        {
+            indexingContext = new IndexingContext();
+        }
+
+        indexingContext.SkipExpensiveSpotifyQueries = !request.AllowExpensiveQueries;
+        indexingContext.SkipExpensiveYouTubeQueries = !request.AllowExpensiveQueries;
+        indexingContext.SkipYouTubeUrlResolving = request.SkipYouTubeUrlResolving;
+
+        var podcast = await _podcastsRepository.GetPodcast(request.PodcastId);
+        if (podcast == null)
+        {
+            throw new ArgumentException($"No podcast found with id '{request.PodcastId}'.");
+        }
+
+        IEnumerable<Episode> episodes = podcast.Episodes;
+        if (request.ReleasedSince.HasValue)
+        {
+            episodes = episodes.Where(x => x.Release >= indexingContext.ReleasedSince);
+        }
+
+        var updated = false;
+        foreach (var episode in episodes)
+        {
+            var criteria = new PodcastServiceSearchCriteria(podcast.Name, string.Empty, podcast.Publisher,
+                episode.Title, episode.Description, episode.Release, episode.Length);
+            if (podcast.AppleId != null && (episode.AppleId == null || episode.Urls.Apple == null))
+            {
+                var match = await _appleUrlCategoriser.Resolve(criteria, podcast, indexingContext);
+                if (match != null)
+                {
+                    episode.Urls.Apple ??= match.Url;
+                    episode.AppleId ??= match.EpisodeId;
+                    _logger.LogInformation($"Enriched from apple: Id: '{match.EpisodeId}', Url: '{match.Url}'.");
+                    updated = true;
+                }
+            }
+
+            if (podcast.YouTubeChannelId != null &&
+                (string.IsNullOrWhiteSpace(episode.YouTubeId) || episode.Urls.YouTube == null))
+            {
+                var match = await _youTubeUrlCategoriser.Resolve(criteria, podcast, indexingContext);
+                if (match != null)
+                {
+                    episode.Urls.YouTube ??= match.Url;
+                    if (string.IsNullOrWhiteSpace(episode.YouTubeId))
+                    {
+                        episode.YouTubeId = match.EpisodeId;
+                    }
+                    _logger.LogInformation($"Enriched from youtube: Id: '{match.EpisodeId}', Url: '{match.Url}'.");
+                    updated = true;
+                }
+            }
+
+            if (podcast.SpotifyId != null &&
+                (string.IsNullOrWhiteSpace(episode.SpotifyId) || episode.Urls.Spotify == null))
+            {
+                var match = await _spotifyUrlCategoriser.Resolve(criteria, podcast, indexingContext);
+                if (match != null)
+                {
+                    episode.Urls.Spotify ??= match.Url;
+                    if (string.IsNullOrWhiteSpace(episode.SpotifyId))
+                    {
+                        episode.SpotifyId= match.EpisodeId;
+                    }
+                    _logger.LogInformation($"Enriched from spotify: Id: '{match.EpisodeId}', Url: '{match.Url}'.");
+                    updated = true;
+                }
+            }
+        }
+
+        if (updated)
+        {
+            await _podcastsRepository.Save(podcast);
+        }
+    }
+}
---
+++ Console-Apps/EnrichExistingEpisodesFromPodcastServices/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using CommandLine;
+using EnrichExistingEpisodesFromPodcastServices;
+using iTunesSearch.Library;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RedditPodcastPoster.Common.Extensions;
+using RedditPodcastPoster.Configuration.Extensions;
+using RedditPodcastPoster.Persistence.Extensions;
+using RedditPodcastPoster.PodcastServices.Apple.Extensions;
+using RedditPodcastPoster.PodcastServices.Extensions;
+using RedditPodcastPoster.PodcastServices.Spotify.Extensions;
+using RedditPodcastPoster.PodcastServices.YouTube.Extensions;
+using RedditPodcastPoster.UrlSubmission.Extensions;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", true)
+    .AddEnvironmentVariables("RedditPodcastPoster_")
+    .AddCommandLine(args)
+    .AddSecrets(Assembly.GetExecutingAssembly());
+
+builder.Services
+    .AddLogging()
+    .AddRepositories(builder.Configuration)
+    .AddScoped<EnrichPodcastEpisodesProcessor>()
+    .AddUrlSubmission()
+    .AddPodcastServices(builder.Configuration)
+    .AddSpotifyServices(builder.Configuration)
+    .AddYouTubeServices(builder.Configuration)
+    .AddAppleServices()
+    .AddRemoteClient()
+    .AddScoped(s => new iTunesSearchManager())
+    .AddHttpClient();
+
+using var host = builder.Build();
+
+return await Parser.Default.ParseArguments<EnrichPodcastEpisodesRequest>(args)
+    .MapResult(async request => await Run(request), errs => Task.FromResult(-1)); // Invalid arguments
+
+async Task<int> Run(EnrichPodcastEpisodesRequest request)
+{
+    var urlSubmitter = host.Services.GetService<EnrichPodcastEpisodesProcessor>()!;
+    await urlSubmitter.Run(request);
+    return 0;
+}
