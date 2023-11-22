de865dc9d1aab00fb2931be2831dc22687fd1963
Jon Breen <jonbreen@outlook.com>
2023-11-22 11:11:00 +0000

    Added lower-case terms

--- Class-Libraries/RedditPodcastPoster.Text/LowerCaseTerms.cs
+++ Class-Libraries/RedditPodcastPoster.Text/LowerCaseTerms.cs
@@ -11,8 +11,9 @@ public static class LowerCaseTerms
         "her", "his", "from", "their", "they", "out", "come", "coming", "away", "by", "what", "who", "made", "make",
         "since", "for", "go", "gone", "give", "gives", "given", "next", "with", "about", "how", "here", "called",
         "call", "doing", "do", "does", "where", "each", "other", "this", "after", "before", "be", "own", "more",
-        "start", "my", "myself", "mine", "get", "gets", "up", "down", "meet", "met", "part", "parts", "ft", "at", 
-        "our", "us", "tell", "why", "don't", "tells", "when", "into", "vs", "only", "off", "end", "being", "re"
+        "start", "my", "myself", "mine", "get", "gets", "up", "down", "meet", "met", "part", "parts", "ft", "at",
+        "our", "us", "tell", "why", "don't", "tells", "when", "into", "vs", "only", "off", "end", "being", "re", "that",
+        "talk", "are", "most", "we", "day", "or", "didn't", "know", "were", "as", "over", "its", "use"
     };
 
     private static readonly string[] AlwaysLowerCaseWords =
