8a3b197d1988fa9a72e6cc9422987bc7ee8aa60c
Jon Breen <jonbreen@outlook.com>
2024-01-22 00:03:04 +0000

    Added console app to determine status of websub

--- Console-Apps/SubjectSeeder/SubjectsSeeder.cs
+++ Console-Apps/SubjectSeeder/SubjectsSeeder.cs
@@ -23,7 +23,10 @@ public class SubjectsSeeder
 
     public async Task Run()
     {
-        var newSubjects = new List<Subject>();
+        var newSubjects = new List<Subject>()
+        {
+            SubjectFactory.Create("S. N. Goenka", "Satya Narayana Goenka, Goenka, Vipassana Meditation, Vipassana")
+        };
         foreach (var subject in newSubjects)
         {
             var match = await _subjectService.Match(subject);
