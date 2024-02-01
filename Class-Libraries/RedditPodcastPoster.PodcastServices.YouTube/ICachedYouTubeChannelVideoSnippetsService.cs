f1837e39b5711fa1edc0edb7ef190f84cc0a7de5
Jon Breen <jonbreen@outlook.com>
2024-02-01 16:53:11 +0000

    Remove redundant interface method with inheritance

--- Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/ICachedYouTubeChannelVideoSnippetsService.cs
+++ Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/ICachedYouTubeChannelVideoSnippetsService.cs
@@ -1,11 +1,7 @@
-using Google.Apis.YouTube.v3.Data;
 using RedditPodcastPoster.PodcastServices.Abstractions;
 
 namespace RedditPodcastPoster.PodcastServices.YouTube;
 
-public interface ICachedYouTubeChannelVideoSnippetsService : IFlushable
+public interface ICachedYouTubeChannelVideoSnippetsService : IYouTubeChannelVideoSnippetsService, IFlushable
 {
-    Task<IList<SearchResult>?> GetLatestChannelVideoSnippets(
-        YouTubeChannelId channelId,
-        IndexingContext indexingContext);
 }
