788306ecd342fb7a431455a7cf0aa44e06decd73
Jon Breen <jonbreen@outlook.com>
2024-03-06 17:55:48 +0000

    Added ability to delete entire podcast's episodes from search-index

--- Console-Apps/DeleteSearchDocument/DeleteSearchDocumentProcessor.cs
+++ Console-Apps/DeleteSearchDocument/DeleteSearchDocumentProcessor.cs
@@ -1,22 +1,43 @@
 ﻿using Azure.Search.Documents;
 using Microsoft.Extensions.Logging;
+using RedditPodcastPoster.Persistence.Abstractions;
 
 namespace DeleteSearchDocument;
 
 public class DeleteSearchDocumentProcessor(
     SearchClient searchClient,
+    IPodcastRepository podcastRepository,
     ILogger<DeleteSearchDocumentProcessor> logger)
 {
     public async Task Process(DeleteSearchDocumentRequest request)
     {
-        var result = await searchClient.DeleteDocumentsAsync(
-            "id",
-            new[] {request.DocumentId.ToString()},
-            new IndexDocumentsOptions {ThrowOnAnyError = true});
-        var success = result.Value.Results.First().Succeeded;
-        if (!success)
+        IEnumerable<Guid> documentIds;
+        if (!request.IsPodcast)
         {
-            logger.LogError(result.Value.Results.First().ErrorMessage);
+            documentIds = new[] {request.DocumentId};
+        }
+        else
+        {
+            var podcast = await podcastRepository.GetBy(x => x.Id == request.DocumentId);
+            if (podcast == null)
+            {
+                throw new ArgumentException($"No podcast found with podcast-id '{request.DocumentId}'.");
+            }
+
+            documentIds = podcast.Episodes.Select(x => x.Id);
+        }
+
+        foreach (var documentId in documentIds)
+        {
+            var result = await searchClient.DeleteDocumentsAsync(
+                "id",
+                new[] {documentId.ToString()},
+                new IndexDocumentsOptions {ThrowOnAnyError = true});
+            var success = result.Value.Results.First().Succeeded;
+            if (!success)
+            {
+                logger.LogError(result.Value.Results.First().ErrorMessage);
+            }
         }
     }
 }
