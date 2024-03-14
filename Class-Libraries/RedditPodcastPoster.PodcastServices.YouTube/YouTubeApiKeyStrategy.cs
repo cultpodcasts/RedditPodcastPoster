32dbbedf617d3b132dc176a2fa9ad96ce5f6b6de
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-03-14 21:48:23 +0000

    Push api-key selection down into strategy (#294)
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

---
+++ Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/YouTubeApiKeyStrategy.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace RedditPodcastPoster.PodcastServices.YouTube;
+
+public class YouTubeApiKeyStrategy(
+    IOptions<YouTubeSettings> settings,
+    ILogger<YouTubeApiKeyStrategy> logger)
+    : IYouTubeApiKeyStrategy
+{
+    private readonly YouTubeSettings _settings = settings.Value;
+
+    public Application GetApplication()
+    {
+        return _settings.Applications.First();
+    }
+}
