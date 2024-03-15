2d5bc793c402fbf4c1301930f7d589722b42d387
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-03-15 16:17:13 +0000

    Handle two keys (#296)
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/YouTubeApiKeyStrategy.cs
+++ Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/YouTubeApiKeyStrategy.cs
@@ -5,13 +5,15 @@ namespace RedditPodcastPoster.PodcastServices.YouTube;
 
 public class YouTubeApiKeyStrategy(
     IOptions<YouTubeSettings> settings,
+#pragma warning disable CS9113 // Parameter is unread.
     ILogger<YouTubeApiKeyStrategy> logger)
+#pragma warning restore CS9113 // Parameter is unread.
     : IYouTubeApiKeyStrategy
 {
     private readonly YouTubeSettings _settings = settings.Value;
 
     public Application GetApplication()
     {
-        return _settings.Applications.First();
+        return _settings.Applications.Skip(DateTime.UtcNow.Hour <= 11 ? 0 : 1).First();
     }
 }
