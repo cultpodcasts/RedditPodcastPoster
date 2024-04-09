22a940d95ed4455ffa1c15e02abd2b58380dc6fd
Jon Breen <jonbreen@outlook.com>
2024-04-09 13:31:00 +0100

    Remove unneeded compiler warnng pragma

--- Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/YouTubeApiKeyStrategy.cs
+++ Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/YouTubeApiKeyStrategy.cs
@@ -5,9 +5,7 @@ namespace RedditPodcastPoster.PodcastServices.YouTube;
 
 public class YouTubeApiKeyStrategy(
     IOptions<YouTubeSettings> settings,
-#pragma warning disable CS9113 // Parameter is unread.
     ILogger<YouTubeApiKeyStrategy> logger)
-#pragma warning restore CS9113 // Parameter is unread.
     : IYouTubeApiKeyStrategy
 {
     private readonly YouTubeSettings _settings = settings.Value;
