8ebc074b8f7de26a7e3e54a663e0afeb99b56d74
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-04-09 13:32:48 +0100

    Feature/log yt application name (#308)
    * Log youtube application name
    
    * Remove unneeded compiler warnng pragma
    
    ---------
    
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/YouTubeApiKeyStrategy.cs
+++ Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/YouTubeApiKeyStrategy.cs
@@ -5,15 +5,15 @@ namespace RedditPodcastPoster.PodcastServices.YouTube;
 
 public class YouTubeApiKeyStrategy(
     IOptions<YouTubeSettings> settings,
-#pragma warning disable CS9113 // Parameter is unread.
     ILogger<YouTubeApiKeyStrategy> logger)
-#pragma warning restore CS9113 // Parameter is unread.
     : IYouTubeApiKeyStrategy
 {
     private readonly YouTubeSettings _settings = settings.Value;
 
     public Application GetApplication()
     {
-        return _settings.Applications.Skip(DateTime.UtcNow.Hour <= 11 ? 0 : 1).First();
+        var application = _settings.Applications.Skip(DateTime.UtcNow.Hour <= 11 ? 0 : 1).First();
+        logger.LogInformation($"{nameof(GetApplication)}: Using application '{application.Name}'.");
+        return application;
     }
 }
