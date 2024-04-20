9df4fca13c084113b2187e767cb618ba8c913674
Jon Breen <jonbreen@outlook.com>
2024-04-20 16:34:42 +0100

    Fix for double-hashtag bug

--- Class-Libraries/RedditPodcastPoster.Text/HashTagEnricher.cs
+++ Class-Libraries/RedditPodcastPoster.Text/HashTagEnricher.cs
@@ -6,8 +6,9 @@ public class HashTagEnricher : IHashTagEnricher
 {
     public (string Title, bool HashTagAdded) AddHashTag(string input, string match, string? replacement = null)
     {
-        var regex = new Regex($"\\b{match}\\b", RegexOptions.IgnoreCase);
-        if (regex.IsMatch(input))
+        var regex = new Regex($"(#?\\b{match}\\b)", RegexOptions.IgnoreCase);
+        var inputMatch = regex.Match(input);
+        if (inputMatch.Success && !inputMatch.Captures.Single().Value.StartsWith("#"))
         {
             return (regex.Replace(input, $"#{replacement ?? match}", 1, 0), true);
         }
