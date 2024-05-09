1d71e2cfa5610ae056c50ad4a95054a41fbb404d
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-05-09 15:33:28 +0100

    Add logging for auth (#316)
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- Cloud/Indexer/Auth/HasScopeHandler.cs
+++ Cloud/Indexer/Auth/HasScopeHandler.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
 
 namespace Indexer.Auth;
 
 public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
 {
+    private readonly ILogger _logger;
+
+    public HasScopeHandler(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger(GetType().FullName);
+    }
+
     protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
     {
+        _logger.LogInformation($"{nameof(HandleRequirementAsync)} initiated.");
         // If user does not have the scope claim, get out of here
         if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
         {
+            _logger.LogWarning(
+                $"{nameof(HandleRequirementAsync)}: No claim of type 'scope' and no issuer matching '{requirement.Issuer}'.");
             return Task.CompletedTask;
         }
 
@@ -19,6 +30,11 @@ public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
         if (scopes.Any(s => s == requirement.Scope))
         {
             context.Succeed(requirement);
+            _logger.LogInformation($"{nameof(HandleRequirementAsync)}: Found claim of scope '{requirement.Scope}'.");
+        }
+        else
+        {
+            _logger.LogWarning($"{nameof(HandleRequirementAsync)}: No claim found of scope '{requirement.Scope}'.");
         }
 
         return Task.CompletedTask;
--- Cloud/Indexer/Auth/ServiceCollectionExtensions.cs
+++ Cloud/Indexer/Auth/ServiceCollectionExtensions.cs
@@ -9,12 +9,16 @@ namespace Indexer.Auth;
 
 public static class ServiceCollectionExtensions
 {
+
     public static IServiceCollection AddAuth0(this IServiceCollection services, IConfiguration config)
     {
         var auth0Settings = config.GetSection("Auth0").Get<Auth0Settings>();
 
         if (auth0Settings != null)
         {
+            Console.Out.WriteLine($"{nameof(AddAuth0)}: Found {nameof(Auth0Settings)}.");
+            Console.Out.WriteLine($"{nameof(AddAuth0)}: Authority: '{auth0Settings.Authority}'.");
+            Console.Out.WriteLine($"{nameof(AddAuth0)}: Audience: '{auth0Settings.Audience}'.");
             services
                 .AddFunctionsAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
