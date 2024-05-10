a104032ca584b76c733357de4c227a072b4a3b0f
Jon Breen <jonbreen@outlook.com>
2024-05-10 16:06:15 +0100

    Reinstated package

--- third-party/darkloop/functions-authorize-4.0.1/test/Abstractions.Tests/Usings.cs
+++
@@ -1,5 +0,0 @@
-// <copyright file="Usings.cs" company="DarkLoop" author="Arturo Martinez">
-//  Copyright (c) DarkLoop. All rights reserved.
-// </copyright>
-
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
