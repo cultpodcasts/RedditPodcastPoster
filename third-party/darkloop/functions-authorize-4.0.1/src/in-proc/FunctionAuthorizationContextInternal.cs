887e136d8730f7f34561d04695d4a733b725bb60
Jon Breen <jonbreen@outlook.com>
2024-05-10 14:00:32 +0100

    Use source of library to track remote azure issue

---
+++ third-party/darkloop/functions-authorize-4.0.1/src/in-proc/FunctionAuthorizationContextInternal.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="FunctionAuthorizationContextInternal.cs" company="DarkLoop" author="Arturo Martinez">
+//  Copyright (c) DarkLoop. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Http;
+
+namespace DarkLoop.Azure.Functions.Authorization
+{
+    /// <summary>
+    /// Internal implementation of the function authorization context.
+    /// </summary>
+    internal sealed class FunctionAuthorizationContextInternal : FunctionAuthorizationContext<HttpContext>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FunctionAuthorizationContextInternal"/> class.
+        /// </summary>
+        internal FunctionAuthorizationContextInternal(
+            string functionName, HttpContext httpContext, AuthorizationPolicy policy, PolicyAuthorizationResult result)
+            : base(functionName, httpContext, policy, result)
+        {
+        }
+    }
+}
---
+++ third-party/darkloop/functions-authorize-4.0.1/src/in-proc/IFunctionsAuthorizationExecutor.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="IFunctionsAuthorizationHandler.cs" company="DarkLoop" author="Arturo Martinez">
+//  Copyright (c) DarkLoop. All rights reserved.
+// </copyright>
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace DarkLoop.Azure.Functions.Authorization
+{
+    /// <summary>
+    /// Executes the authorization for a given function.
+    /// </summary>
+    internal interface IFunctionsAuthorizationExecutor
+    {
+        /// <summary>
+        /// Executes the authorization for a given function.
+        /// </summary>
+        /// <param name="context">The function authorization context.</param>
+        /// <param name="httpContext">The request context.</param>
+        Task ExecuteAuthorizationAsync(FunctionExecutingContext context, HttpContext httpContext);
+    }
+}
