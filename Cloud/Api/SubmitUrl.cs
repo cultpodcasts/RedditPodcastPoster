082f060c07857a71b04d6b0463b59329fcdbb209
Jon Breen <jonbreen@outlook.com>
2024-05-11 15:56:12 +0100

    Attempt to fix remote bug

--- Cloud/Api/SubmitUrl.cs
+++ Cloud/Api/SubmitUrl.cs
@@ -15,7 +15,7 @@ namespace Api;
 [FunctionAuthorize(JwtBearerDefaults.AuthenticationScheme)]
 public class SubmitUrl(IUrlSubmitter urlSubmitter, ILogger<SubmitUrl> logger)
 {
-    [Authorize(Policies.Submit)]
+    [Authorize(Policies.Submit, AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Function("SubmitUrl")]
     public async Task<HttpResponseData> Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "post")] [FromBody]
