b4528692dad6d5ad985403d99788ffbc315b0a6a
Jon Breen <jonbreen@outlook.com>
2024-05-13 00:10:05 +0100

    Add env-var iconfig

--- Cloud/Indexer/Program.cs
+++ Cloud/Indexer/Program.cs
@@ -15,6 +15,7 @@ var host = new HostBuilder()
     //})
     .ConfigureAppConfiguration(builder =>
     {
+        builder.AddEnvironmentVariables();
 #if DEBUG
         builder.AddJsonFile("local.settings.json", false);
         builder.AddConfiguration(new ConfigurationBuilder().AddToConfigurationBuilder<Program>());
