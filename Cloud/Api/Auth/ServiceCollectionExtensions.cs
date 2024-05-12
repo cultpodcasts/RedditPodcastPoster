5e8a4c0a72f07b8b271864193ff78cbcbae3b91f
Jon Breen <jonbreen@outlook.com>
2024-05-12 13:46:32 +0100

    reinstate confiig

--- Cloud/Api/Auth/ServiceCollectionExtensions.cs
+++ Cloud/Api/Auth/ServiceCollectionExtensions.cs
@@ -19,9 +19,9 @@ public static class ServiceCollectionExtensions
             services
                 .AddFunctionsAuthentication(options =>
                 {
-                    //options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    //options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                    //options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
                 })
                 .AddJwtBearer( options =>
                 {
