6aff974cf4a0145db495690c337dc89f7b66d466
Jon Breen <jonbreen@outlook.com>
2024-05-13 15:50:39 +0100

    Probe remote function

--- Cloud/Api/Test.cs
+++ Cloud/Api/Test.cs
@@ -2,16 +2,20 @@ using Api.Dtos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RedditPodcastPoster.Persistence;
 using System.Net;
 
 namespace Api;
 
 public class Test
 {
+    private readonly CosmosDbSettings _settings;
     private readonly ILogger<Test> _logger;
 
-    public Test(ILogger<Test> logger)
+    public Test(IOptions<CosmosDbSettings> settings, ILogger<Test> logger)
     {
+        _settings = settings?.Value ?? throw new ArgumentNullException(nameof(settings));
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
@@ -21,6 +25,8 @@ public class Test
         HttpRequestData req,
         FunctionContext executionContext)
     {
+        _logger.LogInformation($"endpoint: '{_settings.Endpoint}', token: '{_settings.AuthKeyOrResourceToken.Substring(Math.Max(0, _settings.AuthKeyOrResourceToken.Length - 10))}'.");
+
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         var success = req.CreateResponse(HttpStatusCode.OK);
         await success.WriteAsJsonAsync(SubmitUrlResponse.Successful("success"));
