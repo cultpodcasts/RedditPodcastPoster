d1dc4bc1dba4cfa84d7c1412fa8e62a27ec75a54
Jon Breen <jonbreen@outlook.com>
2024-05-13 19:30:20 +0100

    Reinstate primary constructor

--- Class-Libraries/RedditPodcastPoster.Persistence/CosmosDbClientFactory.cs
+++ Class-Libraries/RedditPodcastPoster.Persistence/CosmosDbClientFactory.cs
@@ -5,36 +5,29 @@ using RedditPodcastPoster.Persistence.Abstractions;
 
 namespace RedditPodcastPoster.Persistence;
 
-public class CosmosDbClientFactory : ICosmosDbClientFactory
+public class CosmosDbClientFactory(
+    IJsonSerializerOptionsProvider jsonSerializerOptionsProvider,
+    IOptions<CosmosDbSettings> settings,
+    ILogger<CosmosDbClientFactory> logger)
+    : ICosmosDbClientFactory
 {
-    private readonly CosmosDbSettings _settings;
-    private readonly IJsonSerializerOptionsProvider _jsonSerializerOptionsProvider;
-    private readonly ILogger<CosmosDbClientFactory> _logger;
-
-    public CosmosDbClientFactory(
-        IJsonSerializerOptionsProvider jsonSerializerOptionsProvider,
-        IOptions<CosmosDbSettings> settings,
-        ILogger<CosmosDbClientFactory> logger)
-    {
-        _jsonSerializerOptionsProvider = jsonSerializerOptionsProvider;
-        _logger = logger;
-        _settings = settings.Value;
-    }
+    private readonly CosmosDbSettings _settings = settings.Value;
 
     public CosmosClient Create()
     {
         var cosmosClientOptions = new CosmosClientOptions
         {
             Serializer =
-                new CosmosSystemTextJsonSerializer(_jsonSerializerOptionsProvider.GetJsonSerializerOptions())
+                new CosmosSystemTextJsonSerializer(jsonSerializerOptionsProvider.GetJsonSerializerOptions())
         };
         if (_settings.UseGateway.HasValue && _settings.UseGateway.Value)
         {
             cosmosClientOptions.ConnectionMode = ConnectionMode.Gateway;
         }
 
-        _logger.LogInformation($"endpoint: '{_settings.Endpoint}', token: '{_settings.AuthKeyOrResourceToken.Substring(Math.Max(0, _settings.AuthKeyOrResourceToken.Length - 10))}'.");
-        _logger.LogInformation($"{nameof(IJsonSerializerOptionsProvider)}: '{_jsonSerializerOptionsProvider}'.");
+        logger.LogInformation(
+            $"endpoint: '{_settings.Endpoint}', token: '{_settings.AuthKeyOrResourceToken.Substring(Math.Max(0, _settings.AuthKeyOrResourceToken.Length - 10))}'.");
+        logger.LogInformation($"{nameof(IJsonSerializerOptionsProvider)}: '{jsonSerializerOptionsProvider}'.");
 
         CosmosClient client = new(
             _settings.Endpoint,
