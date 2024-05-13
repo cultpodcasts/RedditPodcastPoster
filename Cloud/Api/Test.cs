344e4998dd39e527b50a5b71d96b4942f0160273
Jon Breen <jonbreen@outlook.com>
2024-05-13 18:50:25 +0100

    Show principle in test endpoint

--- Cloud/Api/Test.cs
+++ Cloud/Api/Test.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using RedditPodcastPoster.Persistence;
 using System.Net;
+using System.Text.Json;
+using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Api;
 
@@ -18,11 +21,41 @@ public class Test(IOptions<CosmosDbSettings> settings, ILogger<Test> logger)
         HttpRequestData req,
         FunctionContext executionContext)
     {
-        logger.LogInformation($"endpoint: '{_settings.Endpoint}', token: '{_settings.AuthKeyOrResourceToken.Substring(Math.Max(0, _settings.AuthKeyOrResourceToken.Length - 10))}'.");
+        var claimsHeader = req.Headers.GetValues("X-MS-CLIENT-PRINCIPAL").SingleOrDefault();
+        if (claimsHeader != null)
+        {
+            var decoded = Convert.FromBase64String(claimsHeader);
+            var json = Encoding.UTF8.GetString(decoded);
+            var principal = JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            var principalJson = JsonSerializer.Serialize(principal);
+            var success = req.CreateResponse(HttpStatusCode.OK);
+            await success.WriteAsJsonAsync(SubmitUrlResponse.Successful($"principle: '{principalJson}'."));
+            return success;
 
-        logger.LogInformation("C# HTTP trigger function processed a request.");
-        var success = req.CreateResponse(HttpStatusCode.OK);
-        await success.WriteAsJsonAsync(SubmitUrlResponse.Successful("success"));
-        return success;
+        }
+        var failure = req.CreateResponse(HttpStatusCode.OK);
+        await failure.WriteAsJsonAsync(SubmitUrlResponse.Failure("No principle."));
+        return failure;
+
     }
+}
+
+public class ClientPrincipal
+{
+    [JsonPropertyName("auth_typ")]
+    public string IdentityProvider { get; set; }
+    [JsonPropertyName("name_typ")]
+    public string NameClaimType { get; set; }
+    [JsonPropertyName("role_typ")]
+    public string RoleClaimType { get; set; }
+    [JsonPropertyName("claims")]
+    public IEnumerable<ClientPrincipalClaim> Claims { get; set; }
+}
+
+public class ClientPrincipalClaim
+{
+    [JsonPropertyName("typ")]
+    public string Type { get; set; }
+    [JsonPropertyName("val")]
+    public string Value { get; set; }
 }
