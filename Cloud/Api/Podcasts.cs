96e6939328d687f15e94cbd15f4b46e3ab718572
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-05-14 09:10:15 +0100

    Feature/podcasts endpooint (#351)
    * Initial version
    
    * Add unauth message
    
    * Improve messaging
    
    * Remove testiing code
    
    * Remove branch deploy
    
    ---------
    
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

---
+++ Cloud/Api/Podcasts.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using Api.Dtos;
+using Api.Extensions;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using RedditPodcastPoster.Persistence.Abstractions;
+
+namespace Api;
+
+public class Podcasts(IPodcastRepository podcastRepository, ILogger<Podcasts> logger)
+{
+    [Function("Podcasts")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get")]
+        HttpRequestData req,
+        FunctionContext executionContext,
+        CancellationToken ct
+    )
+    {
+        if (req.HasScope("submit"))
+        {
+            try
+            {
+                var podcasts = podcastRepository.GetAllBy(
+                    podcast => !podcast.Removed.IsDefined() || podcast.Removed == false,
+                    podcast => new {id = podcast.Id, name = podcast.Name});
+
+                var success = req.CreateResponse(HttpStatusCode.OK);
+                await success.WriteAsJsonAsync(await podcasts.ToListAsync(ct), ct);
+                return success;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"{nameof(Run)}: Failed to get-podcasts.");
+            }
+
+            var failure = req.CreateResponse(HttpStatusCode.InternalServerError);
+            await failure.WriteAsJsonAsync(SubmitUrlResponse.Failure("Unable to retrieve podcasts"), ct);
+            return failure;
+        }
+        else
+        {
+            var failure = req.CreateResponse(HttpStatusCode.Forbidden);
+            await failure.WriteAsJsonAsync(SubmitUrlResponse.Failure("Unauthorised"), ct);
+            return failure;
+        }
+    }
+}
