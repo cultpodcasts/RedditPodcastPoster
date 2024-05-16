a46167474765d51d6a07884871bea650720384bb
Jon Breen <jonbreen@outlook.com>
2024-05-16 20:29:13 +0100

    Split file

---
+++ Class-Libraries/RedditPodcastPoster.Discovery/DiscoveryResultsRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using RedditPodcastPoster.Persistence.Abstractions;
+
+namespace RedditPodcastPoster.Discovery;
+
+public class DiscoveryResultsRepository(
+    IDataRepository repository,
+    ILogger<DiscoveryResultsRepository> logger) : IDiscoveryResultsRepository
+{
+    public Task Save(DiscoveryResultsDocument discoveryResultsDocument)
+    {
+        return repository.Write(discoveryResultsDocument);
+    }
+}
--- Class-Libraries/RedditPodcastPoster.Discovery/IDiscoveryResultsRepository.cs
+++ Class-Libraries/RedditPodcastPoster.Discovery/IDiscoveryResultsRepository.cs
@@ -1,20 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
-using RedditPodcastPoster.Models;
-using RedditPodcastPoster.Persistence.Abstractions;
-
-namespace RedditPodcastPoster.Discovery;
+﻿namespace RedditPodcastPoster.Discovery;
 
 public interface IDiscoveryResultsRepository
 {
     Task Save(DiscoveryResultsDocument discoveryResultsDocument);
-}
-
-public class DiscoveryResultsRepository(
-    IDataRepository repository,
-    ILogger<DiscoveryResultsRepository> logger) : IDiscoveryResultsRepository
-{
-    public Task Save(DiscoveryResultsDocument discoveryResultsDocument)
-    {
-            return repository.Write(discoveryResultsDocument);
-    }
 }
