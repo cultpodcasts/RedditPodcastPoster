8ee2b0b2c92e5c50c6d4e3f9f5f272c15725a7a4
Jon Breen <jonbreen@outlook.com>
2024-05-17 14:51:44 +0100

    Change trigger time

--- Cloud/Discovery/DiscoveryTrigger.cs
+++ Cloud/Discovery/DiscoveryTrigger.cs
@@ -8,7 +8,7 @@ namespace Discovery;
 public class DiscoveryTrigger(ILogger<DiscoveryTrigger> logger)
 {
     [Function("DiscoveryTrigger")]
-    public async Task Run([TimerTrigger("50 */1 * * *"
+    public async Task Run([TimerTrigger("10 */1 * * *"
 #if DEBUG
             , RunOnStartup = true
 #endif
