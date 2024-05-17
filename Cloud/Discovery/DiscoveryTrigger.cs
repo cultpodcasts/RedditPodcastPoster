74bd26889f7f371426cf1c8c27140dc0f5831082
Jon Breen <jonbreen@outlook.com>
2024-05-17 15:29:56 +0100

    Prep for live test

--- Cloud/Discovery/DiscoveryTrigger.cs
+++ Cloud/Discovery/DiscoveryTrigger.cs
@@ -8,7 +8,7 @@ namespace Discovery;
 public class DiscoveryTrigger(ILogger<DiscoveryTrigger> logger)
 {
     [Function("DiscoveryTrigger")]
-    public async Task Run([TimerTrigger("20 */6 * * *"
+    public async Task Run([TimerTrigger("35 */1 * * *"
 #if DEBUG
             , RunOnStartup = true
 #endif
