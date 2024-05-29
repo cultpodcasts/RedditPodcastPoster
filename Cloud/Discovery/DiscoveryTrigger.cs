3cd20afd4715f66a1f6081bacd1c5ecda15a0fbe
Jon Breen <jonbreen@outlook.com>
2024-05-29 13:09:27 +0100

    Reinstate original cron

--- Cloud/Discovery/DiscoveryTrigger.cs
+++ Cloud/Discovery/DiscoveryTrigger.cs
@@ -8,7 +8,7 @@ namespace Discovery;
 public class DiscoveryTrigger(ILogger<DiscoveryTrigger> logger)
 {
     [Function("DiscoveryTrigger")]
-    public async Task Run([TimerTrigger("50 11/12 * * *"
+    public async Task Run([TimerTrigger("30 9/6 * * *"
 #if DEBUG
             , RunOnStartup = true
 #endif
