3c31e25f914829d2b022cab82e2f165b707ee487
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-06-08 20:04:39 +0100

    Feature/discovery ignore term configuration (#396)
    * Restore cron
    
    * Moved ignore-terms to configuration
    
    ---------
    
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

---
+++ Class-Libraries/RedditPodcastPoster.Discovery/IgnoreTermsSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace RedditPodcastPoster.Discovery;
+
+public class IgnoreTermsSettings
+{
+    public string[]? IgnoreTerms { get; set; }
+
+    public override string ToString()
+    {
+        if (IgnoreTerms == null)
+        {
+            return "No ignore terms found in configuration";
+        }
+
+        return string.Join(", ", IgnoreTerms.Select(x => $"'{x}'"));
+    }
+}
