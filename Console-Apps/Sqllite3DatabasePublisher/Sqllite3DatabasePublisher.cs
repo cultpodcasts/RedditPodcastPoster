30b7c7b4db4290fa4519204010becec559846286
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-06-12 11:01:41 +0100

    Feature/d1 for seo data (#397)
    * Restore cron
    
    * Update sql publisher for seo
    
    * Adjusted config + function timeout
    
    ---------
    
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- Console-Apps/Sqllite3DatabasePublisher/Sqllite3DatabasePublisher.cs
+++ Console-Apps/Sqllite3DatabasePublisher/Sqllite3DatabasePublisher.cs
@@ -1,55 +1,73 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Extensions.Logging;
 using RedditPodcastPoster.Persistence.Abstractions;
 
 namespace Sqllite3DatabasePublisher;
 
 public class Sqllite3DatabasePublisher(
     IPodcastRepository podcastRepository,
-    PodcastContext podcastContext,
+    ISubjectRepository subjectRepository,
+    DatabaseContext databaseContext,
 #pragma warning disable CS9113 // Parameter is unread.
     ILogger<Sqllite3DatabasePublisher> logger
 #pragma warning restore CS9113 // Parameter is unread.
-) {
-    public async Task Run()
+)
+{
+    public async Task Run(Request request)
     {
-        await podcastContext.Database.EnsureCreatedAsync();
-        var podcasts = await podcastRepository.GetAll().ToListAsync();
+        await databaseContext.Database.EnsureCreatedAsync();
+        await PopulatePodcasts();
+        await PopulateSubjects();
+        await databaseContext.SaveChangesAsync();
 
-        foreach (var podcast in podcasts)
-        {
-            var entity = new Podcast
-            {
-                Guid = podcast.Id,
-                PrimaryPostService = podcast.PrimaryPostService,
-                Name = podcast.Name,
-                Publisher = podcast.Publisher,
-                Episodes = podcast.Episodes
-                    .Where(episode => !episode.Removed)
-                    .Select(episode => new Episode
-                    {
-                        Guid = episode.Id,
-                        Release = episode.Release,
-                        Length = episode.Length,
-                        Explicit = episode.Explicit,
-                        Title = episode.Title,
-                        Description = episode.Description,
-                        Subjects = string.Join(",", episode.Subjects),
-                        YouTube = episode.Urls.YouTube,
-                        Spotify = episode.Urls.Spotify,
-                        Apple = episode.Urls.Apple
-                    }).ToList()
-            };
-            podcastContext.Podcasts.Add(entity);
-            await podcastContext.SaveChangesAsync();
-        }
 
-        var query = from b in podcastContext.Podcasts
+        var query = from b in databaseContext.Podcasts
             select b;
 
         Console.WriteLine("All podcasts in the database:");
         foreach (var item in query)
         {
             Console.WriteLine(item.Guid);
+        }
+    }
+
+    private async Task PopulatePodcasts()
+    {
+        var podcasts = await podcastRepository
+            .GetAllBy(
+                podcast =>
+                    ((!podcast.Removed.IsDefined() || podcast.Removed == false) &&
+                     podcast.IndexAllEpisodes) || podcast.EpisodeIncludeTitleRegex != "",
+                podcast => new { id = podcast.Id, name = podcast.Name })
+            .ToListAsync();
+
+        foreach (var podcast in podcasts)
+        {
+            logger.LogInformation(System.Text.Json.JsonSerializer.Serialize(podcast));
+
+            var entity = new Podcast
+            {
+                Guid = podcast.id,
+                Name = podcast.name
+            };
+            databaseContext.Podcasts.Add(entity);
+        }
+    }
+
+    private async Task PopulateSubjects()
+    {
+        var subjects = await subjectRepository.GetAll().ToListAsync();
+
+        foreach (var subject in subjects)
+        {
+            logger.LogInformation(System.Text.Json.JsonSerializer.Serialize(subject));
+
+            var entity = new Subject
+            {
+                Guid = subject.Id,
+                Name = subject.Name
+            };
+            databaseContext.Subjects.Add(entity);
         }
     }
 }
---
+++ Console-Apps/Sqllite3DatabasePublisher/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sqllite3DatabasePublisher;
+
+public class Subject
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] public int Id { get; set; }
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
+
+}
+
