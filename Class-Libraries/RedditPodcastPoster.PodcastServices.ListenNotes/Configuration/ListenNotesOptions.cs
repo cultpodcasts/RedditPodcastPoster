0e35d07739e73ea9f4deaa892419f5ee251740a9
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-06-15 10:52:45 +0100

    Feature/remove pub since on listennotes api call (#405)
    * Removed published_after from listen-notes-api call
    
    * Add confguration for using published-after on listen-notes query
    
    ---------
    
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- Class-Libraries/RedditPodcastPoster.PodcastServices.ListenNotes/Configuration/ListenNotesOptions.cs
+++ Class-Libraries/RedditPodcastPoster.PodcastServices.ListenNotes/Configuration/ListenNotesOptions.cs
@@ -3,6 +3,7 @@
 public class ListenNotesOptions
 {
     public required string Key { get; set; }
-    
+
     public required int RequestDelaySeconds { get; set; }
+    public bool UsePublishedAfter { get; set; } = false;
 }
