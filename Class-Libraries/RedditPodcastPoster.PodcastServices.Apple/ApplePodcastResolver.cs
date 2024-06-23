b4ad0df52bd00aa3724f2b6381ba25a316247ec5
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-06-23 15:14:52 +0100

    Exception handling around itunessearchmanager (#420)
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- Class-Libraries/RedditPodcastPoster.PodcastServices.Apple/ApplePodcastResolver.cs
+++ Class-Libraries/RedditPodcastPoster.PodcastServices.Apple/ApplePodcastResolver.cs
@@ -18,20 +18,36 @@ public class ApplePodcastResolver(
         Podcast? matchingPodcast = null;
         if (request.PodcastAppleId != null)
         {
-            var podcastResult = await iTunesSearchManager.GetPodcastById(request.PodcastAppleId.Value);
-            matchingPodcast = podcastResult.Podcasts.FirstOrDefault();
+            try
+            {
+                var podcastResult = await iTunesSearchManager.GetPodcastById(request.PodcastAppleId.Value);
+                matchingPodcast = podcastResult.Podcasts.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex,
+                    $"Error invoking {nameof(iTunesSearchManager.GetPodcastById)} with id '{request.PodcastAppleId.Value}'.");
+            }
         }
 
         if (matchingPodcast == null)
         {
-            var items = await iTunesSearchManager.GetPodcasts(request.PodcastName, PodcastSearchLimit);
-            IEnumerable<Podcast> podcasts = items.Podcasts;
-            if (podcasts.Count() > 1)
+            try
             {
-                podcasts = podcasts.Where(x => x.ArtistName.ToLower().Trim() == request.PodcastPublisher.ToLower());
-            }
+                var items = await iTunesSearchManager.GetPodcasts(request.PodcastName, PodcastSearchLimit);
+                IEnumerable<Podcast> podcasts = items.Podcasts;
+                if (podcasts.Count() > 1)
+                {
+                    podcasts = podcasts.Where(x => x.ArtistName.ToLower().Trim() == request.PodcastPublisher.ToLower());
+                }
 
-            matchingPodcast = podcasts.FirstOrDefault(x => x.Name.ToLower() == request.PodcastName.ToLower());
+                matchingPodcast = podcasts.FirstOrDefault(x => x.Name.ToLower() == request.PodcastName.ToLower());
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e,
+                    $"Error invoking {nameof(iTunesSearchManager.GetPodcasts)} with podcast-name '{request.PodcastName}'.");
+            }
         }
 
         return matchingPodcast;
