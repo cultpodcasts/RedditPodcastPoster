9c603e51ed960a3802eadb0ffd4247ac87c5d151
Jon Breen <jonbreen@outlook.com>
2024-07-28 11:54:21 +0100

    Refactor unposted episode search into podcast-repository

--- Class-Libraries/RedditPodcastPoster.Common/Episodes/EpisodeProcessor.cs
+++ Class-Libraries/RedditPodcastPoster.Common/Episodes/EpisodeProcessor.cs
@@ -1,8 +1,5 @@
-﻿using System.Linq.Expressions;
-using Microsoft.Azure.Cosmos.Linq;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using RedditPodcastPoster.Common.Adaptors;
-using RedditPodcastPoster.Models;
 using RedditPodcastPoster.Persistence.Abstractions;
 
 namespace RedditPodcastPoster.Common.Episodes;
@@ -14,23 +11,13 @@ public class EpisodeProcessor(
     ILogger<EpisodeProcessor> logger)
     : IEpisodeProcessor
 {
-    public static Expression<Func<Podcast, bool>> RecentRelease = x =>
-        (!x.Removed.IsDefined() || x.Removed == false) &&
-        x.Episodes.Any(
-            episode =>
-                episode.Release > DateTime.Now.AddDays(-30) &&
-                episode.Posted == false &&
-                episode.Ignored == false &&
-                episode.Removed == false);
-
-
     public async Task<ProcessResponse> PostEpisodesSinceReleaseDate(
         DateTime since,
         bool youTubeRefreshed,
         bool spotifyRefreshed)
     {
         logger.LogInformation($"{nameof(PostEpisodesSinceReleaseDate)} Finding episodes released since '{since}'.");
-        var podcasts = await podcastRepository.GetAllBy(RecentRelease).ToArrayAsync();
+        var podcasts = await podcastRepository.GetPodcastsWithUnpostedEpisodesReleasedSince(since).ToArrayAsync();
 
         var matchingPodcastEpisodeResults =
             await podcastEpisodesPoster.PostNewEpisodes(since, podcasts, youTubeRefreshed, spotifyRefreshed);
