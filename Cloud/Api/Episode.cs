56a429204f58e0c26b0e1356f5fc8d230f3b5730
Jon Breen <jonbreen@outlook.com>
2024-08-02 15:30:16 +0100

    Added Episode endpoint (get)

---
+++ Cloud/Api/Episode.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using Api.Dtos;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RedditPodcastPoster.Persistence.Abstractions;
+
+namespace Api;
+
+public class Episode(
+    IPodcastRepository podcastRepository,
+    ILogger<Episode> logger,
+    ILogger<BaseHttpFunction> baseLogger,
+    IOptions<HostingOptions> hostingOptions)
+    : BaseHttpFunction(hostingOptions, baseLogger)
+{
+    [Function("Episode")]
+    public Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "episode/{episodeId:guid}")]
+        HttpRequestData req,
+        Guid episodeId,
+        FunctionContext executionContext,
+        CancellationToken ct
+    )
+    {
+        return HandleRequest(req, ["submit"], episodeId, Get, Unauthorised, ct);
+    }
+
+    private async Task<HttpResponseData> Get(HttpRequestData req, Guid episodeId, CancellationToken c)
+    {
+        try
+        {
+            var podcast = await podcastRepository.GetBy(x => x.Episodes.Any(ep => ep.Id == episodeId));
+            var episode = podcast?.Episodes.SingleOrDefault(x => x.Id == episodeId);
+
+            if (episode == null)
+            {
+                return req.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            var success = await req.CreateResponse(HttpStatusCode.OK)
+                .WithJsonBody(episode, c);
+            return success;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, $"{nameof(Run)}: Failed to get-podcasts.");
+        }
+
+        var failure = await req.CreateResponse(HttpStatusCode.InternalServerError)
+            .WithJsonBody(SubmitUrlResponse.Failure("Unable to retrieve podcasts"), c);
+        return failure;
+    }
+}
