f1aaccb32110992b95cb9b4b87fa8da54bcfb2a2
Jon Breen <jonbreen@outlook.com>
2024-08-05 16:37:24 +0100

    Add podcast-name to Episode-Dto

---
+++ Cloud/Api/Dtos/DiscreteEpisode.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Api.Dtos;
+
+public class DiscreteEpisode : RedditPodcastPoster.Models.Episode
+{
+    [JsonPropertyName("podcastName")]
+    [JsonPropertyOrder(30)]
+    public string PodcastName { get; set; } = "";
+
+}
