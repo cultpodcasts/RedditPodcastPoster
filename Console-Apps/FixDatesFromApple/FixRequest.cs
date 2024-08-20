d7e520fd5af5cd1981a1b2fcc9902c02dc000b44
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-08-20 13:32:10 +0100

    Added app to fix dates from apple (#485)

---
+++ Console-Apps/FixDatesFromApple/FixRequest.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace FixDatesFromApple;
+
+public class FixRequest
+{
+    [Value(0, Required = true, HelpText = "The id of the podcast to index")]
+    public Guid PodcastId { get; set; }
+
+    [Value(1, Required = true, HelpText = "The incorrect date to fix")]
+    public DateOnly Date { get; set; }
+}
