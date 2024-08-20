50ecbf225b7d9c2e493f4e8b4c060449506fd4c2
Jon Breen <jonbreen@outlook.com>
2024-08-20 13:30:28 +0100

    Added app to fix dates from apple

---
+++ Console-Apps/FixDatesFromApple/Processor.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using RedditPodcastPoster.Persistence.Abstractions;
+using RedditPodcastPoster.PodcastServices.Abstractions;
+using RedditPodcastPoster.PodcastServices.Apple;
+
+namespace FixDatesFromApple;
+
+public class Processor(
+    IPodcastRepository podcastRepository,
+    IAppleEpisodeProvider appleEpisodeProvider,
+    ILogger<Processor> logger
+)
+{
+    public async Task Run(FixRequest request)
+    {
+        var podcast = await podcastRepository.GetPodcast(request.PodcastId);
+        if (podcast == null || !podcast.AppleId.HasValue)
+        {
+            throw new ArgumentException("Podcast is null or has no apple-id");
+        }
+
+        var appleEpisodes = await appleEpisodeProvider.GetEpisodes(
+            new ApplePodcastId(podcast.AppleId.Value),
+            new IndexingContext());
+        var updated = false;
+
+        var matchingEpisodes = podcast.Episodes.Where(x => DateOnly.FromDateTime(x.Release) == request.Date);
+        logger.LogInformation($"There are '{matchingEpisodes.Count()}' episodes matching date");
+        foreach (var episode in matchingEpisodes)
+        {
+            if (episode.AppleId.HasValue)
+            {
+                var appleEpisode = appleEpisodes.SingleOrDefault(x => x.AppleId == episode.AppleId.Value);
+                if (appleEpisode != null)
+                {
+                    logger.LogInformation($"Updating '{episode.Title}' to '{appleEpisode.Release:G}'.");
+                    episode.Release = appleEpisode.Release;
+                    updated = true;
+                }
+                else
+                {
+                    logger.LogWarning($"'{episode.Title}' has no matching episode'.");
+                }
+            }
+            else
+            {
+                logger.LogWarning($"'{episode.Title}' has no apple-id'.");
+
+            }
+        }
+
+        if (updated)
+        {
+            await podcastRepository.Save(podcast);
+        }
+    }
+}
---
+++ Console-Apps/FixDatesFromApple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using CommandLine;
+using FixDatesFromApple;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RedditPodcastPoster.Configuration.Extensions;
+using RedditPodcastPoster.Persistence.Extensions;
+using RedditPodcastPoster.PodcastServices.Apple.Extensions;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", true)
+    .AddEnvironmentVariables("RedditPodcastPoster_")
+    .AddCommandLine(args)
+    .AddSecrets(Assembly.GetExecutingAssembly());
+
+builder.Services
+    .AddLogging()
+    .AddSingleton<Processor>()
+    .AddRepositories()
+    .AddAppleServices()
+    .AddHttpClient();
+
+
+using var host = builder.Build();
+
+return await Parser.Default.ParseArguments<FixRequest>(args)
+    .MapResult(async indexRequest => await Run(indexRequest), errs => Task.FromResult(-1)); // Invalid arguments
+
+async Task<int> Run(FixRequest request)
+{
+    var urlSubmitter = host.Services.GetService<Processor>()!;
+    await urlSubmitter.Run(request);
+    return 0;
+}
