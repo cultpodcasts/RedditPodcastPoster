61a87cb91d53df3c12e8efa521013eba895d83a3
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-09-11 17:25:19 +0100

    Handle non link posts in recent posts list when removing post (#498)

--- Class-Libraries/RedditPodcastPoster.Reddit/PostManager.cs
+++ Class-Libraries/RedditPodcastPoster.Reddit/PostManager.cs
@@ -17,11 +17,15 @@ public class PostManager(
     public async Task RemoveEpisodePost(PodcastEpisode podcastEpisode)
     {
         var subredditPosts = redditClient.Subreddit(_subredditSettings.SubredditName).Posts.New;
-        var subredditEpisodePosts = subredditPosts.Cast<LinkPost>().Where(x =>
-            x != null && x.Author == redditClient.Account.Me.Name &&
-            ((podcastEpisode.Episode.Urls.Apple != null && x.URL == podcastEpisode.Episode.Urls.Apple.ToString()) ||
-             (podcastEpisode.Episode.Urls.Spotify != null && x.URL == podcastEpisode.Episode.Urls.Spotify.ToString()) ||
-             (podcastEpisode.Episode.Urls.YouTube != null && x.URL == podcastEpisode.Episode.Urls.YouTube.ToString())));
+        var subredditEpisodePosts = subredditPosts
+            .OfType<LinkPost>()
+            .Where(x =>
+                x != null && x.Author == redditClient.Account.Me.Name &&
+                ((podcastEpisode.Episode.Urls.Apple != null && x.URL == podcastEpisode.Episode.Urls.Apple.ToString()) ||
+                 (podcastEpisode.Episode.Urls.Spotify != null &&
+                  x.URL == podcastEpisode.Episode.Urls.Spotify.ToString()) ||
+                 (podcastEpisode.Episode.Urls.YouTube != null &&
+                  x.URL == podcastEpisode.Episode.Urls.YouTube.ToString())));
         if (subredditEpisodePosts.Any())
         {
             foreach (var subredditEpisodePost in subredditEpisodePosts)
