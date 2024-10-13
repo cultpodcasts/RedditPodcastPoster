b589d9710b053459983ce92a321ff3dab633dcaf
Jon Breen <jonbreen@outlook.com>
2024-10-13 17:28:27 +0100

    Added tests around change

---
+++ Class-Libraries/RedditPodcastPoster.Twitter.Tests/HashTagProviderTests.cs
@@ -0,0 +1,35 @@
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using Moq.AutoMock;
+using RedditPodcastPoster.Models;
+using RedditPodcastPoster.Persistence.Abstractions;
+
+namespace RedditPodcastPoster.Twitter.Tests;
+
+public class HashTagProviderTests
+{
+    private readonly Fixture _fixture = new();
+    private readonly AutoMocker _mocker = new();
+
+    private HashTagProvider Sut => _mocker.CreateInstance<HashTagProvider>();
+
+    [Theory]
+    [InlineData("Term A", "#TermA")]
+    [InlineData("Term's A", "#TermsA")]
+    [InlineData("Term-A", "#TermA")]
+    public async Task GetHashTags_WithEnrichmentHashTag_IsCorrect(string enrichmentHashTag, string expected)
+    {
+        // arrange
+        var subject = new Subject(_fixture.Create<string>())
+        {
+            EnrichmentHashTags = [enrichmentHashTag]
+        };
+        _mocker.GetMock<ISubjectRepository>().Setup(x => x.GetByName(It.IsAny<string>()))
+            .ReturnsAsync(subject);
+        // act
+        var result = await Sut.GetHashTags([_fixture.Create<string>()]);
+        // assert
+        result.Should().BeEquivalentTo([new HashTag(enrichmentHashTag, expected)]);
+    }
+}
