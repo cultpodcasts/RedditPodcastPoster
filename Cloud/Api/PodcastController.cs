ce832e002e4fb099e1285bfa8e1f545087ff5d86
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-10-16 15:31:49 +0100

    Update correct podcast whens split (#526)
    * Update correct podcast whens split
    
    * Adjust new method

--- Cloud/Api/PodcastController.cs
+++ Cloud/Api/PodcastController.cs
@@ -247,7 +247,7 @@ public class PodcastController(
         {
             logger.LogInformation($"{nameof(Get)}: Get podcast with name '{podcastName}'.");
             podcastName = WebUtility.UrlDecode(podcastName);
-            var podcast = await podcastRepository.GetBy(x => x.Name == podcastName);
+            var podcast = await GetPodcast(podcastName, c);
             if (podcast != null)
             {
                 var dto = new Podcast
@@ -285,6 +285,22 @@ public class PodcastController(
         var failure = await req.CreateResponse(HttpStatusCode.InternalServerError)
             .WithJsonBody(SubmitUrlResponse.Failure("Unable to retrieve podcast"), c);
         return failure;
+    }
+
+    private async Task<RedditPodcastPoster.Models.Podcast?> GetPodcast(string podcastName, CancellationToken c)
+    {
+        var podcasts = await podcastRepository.GetAllBy(x => x.Name == podcastName).ToListAsync(c);
+        RedditPodcastPoster.Models.Podcast? podcast = null;
+        if (podcasts.Count() == 1)
+        {
+            podcast = podcasts.Single();
+        }
+        else if (podcasts.Count() > 1)
+        {
+            podcast = podcasts.SingleOrDefault(x => x.IndexAllEpisodes);
+        }
+
+        return podcast;
     }
 
     private async Task<HttpResponseData> Index(HttpRequestData req, string podcastName, CancellationToken c)
