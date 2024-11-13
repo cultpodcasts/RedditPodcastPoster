23bf364f5b583e2e41ed688768fdbd794c73dc0f
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-11-13 19:42:13 +0000

    Ui-enhancements (#574)

--- Cloud/Api/Dtos/DiscreteEpisode.cs
+++ Cloud/Api/Dtos/DiscreteEpisode.cs
@@ -8,4 +8,16 @@ public class DiscreteEpisode : Episode
     [JsonPropertyName("podcastName")]
     [JsonPropertyOrder(30)]
     public string PodcastName { get; set; } = "";
+
+    [JsonPropertyName("youTubePodcast")]
+    [JsonPropertyOrder(200)]
+    public bool YouTubePodcast { get; set; }
+
+    [JsonPropertyName("spotifyPodcast")]
+    [JsonPropertyOrder(201)]
+    public bool SpotifyPodcast { get; set; }
+
+    [JsonPropertyName("applePodcast")]
+    [JsonPropertyOrder(202)]
+    public bool ApplePodcast { get; set; }
 }
