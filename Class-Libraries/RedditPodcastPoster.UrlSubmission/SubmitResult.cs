db80d6498072a4e112b579f7357e5e38c560068e
Jon Breen <jonbreen@outlook.com>
2024-11-15 15:05:24 +0000

    Added details to submit console app

--- Class-Libraries/RedditPodcastPoster.UrlSubmission/SubmitResult.cs
+++ Class-Libraries/RedditPodcastPoster.UrlSubmission/SubmitResult.cs
@@ -17,4 +17,30 @@ public record SubmitResult(
         PodcastRemoved,
         EpisodeAlreadyExists
     }
+
+    public override string ToString()
+    {
+        var results = new List<string>
+        {
+            $"Podcast-Result: '{PodcastResult.ToString()}'",
+            $"episode-Result: '{EpisodeResult.ToString()}'"
+        };
+
+        if (EpisodeId != null)
+        {
+            results.Add($"episode-id: '{EpisodeId}'");
+        }
+
+        if (SubmitEpisodeDetails != null)
+        {
+            results.Add(
+                $"spotify: {SubmitEpisodeDetails.Spotify}, apple: {SubmitEpisodeDetails.Apple}, youtube: {SubmitEpisodeDetails.YouTube}");
+            if (SubmitEpisodeDetails.Subjects != null)
+            {
+                results.Add($"subjects: '{string.Join("', '", SubmitEpisodeDetails.Subjects)}'");
+            }
+        }
+
+        return string.Join(", ", results);
+    }
 }
