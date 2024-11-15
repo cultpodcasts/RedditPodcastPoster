1d58277cfaa9a6926a3fe50238487d4292a9053b
Jon Breen <jonbreen@outlook.com>
2024-11-15 19:47:55 +0000

    Surface details of episode changes when indexing thru api

---
+++ Cloud/Api/Dtos/IndexedEpisode.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Api.Dtos;
+
+public class IndexedEpisode
+{
+    [JsonPropertyName("episodeId")]
+    public required Guid EpisodeId { get; set; }
+
+    [JsonPropertyName("spotify")]
+    public required bool Spotify { get; set; }
+
+    [JsonPropertyName("apple")]
+    public required bool Apple { get; set; }
+
+    [JsonPropertyName("youtube")]
+    public required bool YouTube { get; set; }
+
+    [JsonPropertyName("subjects")]
+    public required string[] Subjects { get; set; }
+}
