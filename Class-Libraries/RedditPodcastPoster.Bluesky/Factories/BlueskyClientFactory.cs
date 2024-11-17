6257b97d5a9b1b48a7858530a5dcadcb15d0f791
Jon Breen <jonbreen@outlook.com>
2024-11-17 18:49:19 +0000

    Use reuse-session

--- Class-Libraries/RedditPodcastPoster.Bluesky/Factories/BlueskyClientFactory.cs
+++ Class-Libraries/RedditPodcastPoster.Bluesky/Factories/BlueskyClientFactory.cs
@@ -1,15 +1,21 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using RedditPodcastPoster.Bluesky.Configuration;
 using X.Bluesky;
 
 namespace RedditPodcastPoster.Bluesky.Factories;
 
-public class BlueskyClientFactory(IOptions<BlueskyOptions> options) : IBlueskyClientFactory
+public class BlueskyClientFactory(
+    IOptions<BlueskyOptions> options,
+    ILogger<BlueskyClientFactory> logger,
+    ILogger<BlueskyClient> blueskyLogger
+) : IBlueskyClientFactory
 {
     private readonly BlueskyOptions _options = options.Value;
 
     public IBlueskyClient Create()
     {
-        return new BlueskyClient(_options.Identifier, _options.Password);
+        logger.LogInformation($"Creating blue-sky client with reuse-session: '{_options.ReuseSession}'.");
+        return new BlueskyClient(_options.Identifier, _options.Password, _options.ReuseSession, blueskyLogger);
     }
 }
