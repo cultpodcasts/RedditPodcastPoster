f53b47ba7b28493b1838c354cace50827c015558
Jon Breen <jonbreen@outlook.com>
2024-11-27 21:53:53 +0000

    Break yt resolving on failure in tolerant yt client

--- Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/ChannelSnippets/TolerantYouTubeChannelVideoSnippetsService.cs
+++ Class-Libraries/RedditPodcastPoster.PodcastServices.YouTube/ChannelSnippets/TolerantYouTubeChannelVideoSnippetsService.cs
@@ -36,6 +36,7 @@ public class TolerantYouTubeChannelVideoSnippetsService(
 
         if (!success)
         {
+            indexingContext.SkipYouTubeUrlResolving = true;
             logger.LogError(
                 "Unable to obtain latest-channel-video-snippets for channel-id '{channelId}'. Attempt: {attempt}.",
                 channelId.ChannelId, reattempt);
