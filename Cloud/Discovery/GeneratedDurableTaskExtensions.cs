b0613d6f2a2d206a2aef32fae1cf4ee5d80ef677
Jon Breen <jonbreen@outlook.com>
2024-11-29 01:17:44 +0000

    Remove durable-task generator

---
+++ Cloud/Discovery/GeneratedDurableTaskExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Discovery;
+
+public static class GeneratedDurableTaskExtensions
+{
+    private static readonly ITaskOrchestrator SingletonOrchestration = new Orchestration();
+
+    [Function(nameof(Orchestration))]
+    public static Task<DiscoveryContext> Orchestration([OrchestrationTrigger] TaskOrchestrationContext context)
+    {
+        return SingletonOrchestration.RunAsync(context, context.GetInput<object>())
+            .ContinueWith(t => (DiscoveryContext) (t.Result ?? default(DiscoveryContext)!),
+                TaskContinuationOptions.ExecuteSynchronously);
+    }
+
+    /// <inheritdoc cref="IOrchestrationSubmitter.ScheduleNewOrchestrationInstanceAsync" />
+    public static Task<string> ScheduleNewOrchestrationInstanceAsync(
+        this IOrchestrationSubmitter client, object input, StartOrchestrationOptions? options = null)
+    {
+        return client.ScheduleNewOrchestrationInstanceAsync("Orchestration", input, options);
+    }
+
+    /// <inheritdoc cref="TaskOrchestrationContext.CallSubOrchestratorAsync" />
+    public static Task<DiscoveryContext> CallOrchestrationAsync(
+        this TaskOrchestrationContext context, object input, TaskOptions? options = null)
+    {
+        return context.CallSubOrchestratorAsync<DiscoveryContext>("Orchestration", input, options);
+    }
+
+    public static Task<DiscoveryContext> CallDiscoverAsync(this TaskOrchestrationContext ctx, DiscoveryContext input,
+        TaskOptions? options = null)
+    {
+        return ctx.CallActivityAsync<DiscoveryContext>("Discover", input, options);
+    }
+
+    [Function(nameof(Discover))]
+    public static async Task<DiscoveryContext> Discover([ActivityTrigger] DiscoveryContext input, string instanceId,
+        FunctionContext executionContext)
+    {
+        ITaskActivity activity = ActivatorUtilities.CreateInstance<Discover>(executionContext.InstanceServices);
+        TaskActivityContext context = new GeneratedActivityContext("Discover", instanceId);
+        var result = await activity.RunAsync(context, input);
+        return (DiscoveryContext) result!;
+    }
+
+    private sealed class GeneratedActivityContext : TaskActivityContext
+    {
+        public GeneratedActivityContext(TaskName name, string instanceId)
+        {
+            Name = name;
+            InstanceId = instanceId;
+        }
+
+        public override TaskName Name { get; }
+
+        public override string InstanceId { get; }
+    }
+}
--- Cloud/Indexer/Orchestration.cs
+++ Cloud/Indexer/Orchestration.cs
@@ -13,6 +13,9 @@ public class HourlyOrchestration : TaskOrchestrator<object, IndexerContext>
             $"{nameof(HourlyOrchestration)}.{nameof(RunAsync)} initiated. Instance-id: '{context.InstanceId}'.");
 
         var indexerContext = new IndexerContext(context.NewGuid());
+
+        await context.CallActivityAsync<string>("Orchestration_Hello", "Tokyo");
+
         indexerContext = await context.CallIndexerAsync(indexerContext);
         logger.LogInformation($"{nameof(Indexer)} complete.");
 
