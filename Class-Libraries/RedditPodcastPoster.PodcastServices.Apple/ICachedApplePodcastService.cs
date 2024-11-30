05b780e9cd6e42ea33e344b08fe14c6232d5490e
Jon Breen <jonbreen@outlook.com>
2024-11-30 19:40:03 +0000

    Better yt mage handling

--- Class-Libraries/RedditPodcastPoster.PodcastServices.Apple/ICachedApplePodcastService.cs
+++ Class-Libraries/RedditPodcastPoster.PodcastServices.Apple/ICachedApplePodcastService.cs
@@ -4,4 +4,6 @@ namespace RedditPodcastPoster.PodcastServices.Apple;
 
 public interface ICachedApplePodcastService : IApplePodcastService, IFlushable
 {
+    Task<AppleEpisode?> SingleUseGetEpisode(ApplePodcastId podcastId, long episodeId,
+        IndexingContext indexingContext);
 }
