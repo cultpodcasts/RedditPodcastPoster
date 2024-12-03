120626935b87ef12c3b3de51b5df9884c68f33f3
Jon Breen <jonbreen@outlook.com>
2024-12-03 01:08:05 +0000

    Fix prod bug

--- Class-Libraries/RedditPodcastPoster.PodcastServices.Spotify/PodcastEpisodesResult.cs
+++ Class-Libraries/RedditPodcastPoster.PodcastServices.Spotify/PodcastEpisodesResult.cs
@@ -6,6 +6,6 @@ public class PodcastEpisodesResult(
     IEnumerable<SimpleEpisode> episodes,
     bool expensiveQueryFound = false)
 {
-    public IEnumerable<SimpleEpisode> Episodes => episodes.Where(x => x.Type == ItemType.Episode);
+    public IEnumerable<SimpleEpisode> Episodes => episodes.Where(x => x?.Type == ItemType.Episode);
     public bool ExpensiveQueryFound { get; } = expensiveQueryFound;
 }
