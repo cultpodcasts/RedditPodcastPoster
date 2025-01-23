70e2196f833c0e43a6dd8dfc8c36de169cfa8bfd
Jon Breen <jonbreen@outlook.com>
2025-01-23 16:29:43 +0000

    Update disscovery r2 content on submit

--- Cloud/Api/Services/IDiscoveryResultsService.cs
+++ Cloud/Api/Services/IDiscoveryResultsService.cs
@@ -8,4 +8,5 @@ public interface IDiscoveryResultsService
     Task<DiscoveryResponse> Get(CancellationToken c);
     Task MarkAsProcessed(Guid[] documentIds, Guid[] acceptedResultIds, Guid[] erroredResultIds);
     Task<IEnumerable<DiscoveryResult>> GetDiscoveryResult(DiscoverySubmitRequest discoverySubmitRequest);
+    Task UpdateDiscoveryInfoContent();
 }
