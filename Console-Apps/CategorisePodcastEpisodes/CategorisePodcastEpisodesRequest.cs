f6d8ed7d3849f122ffda979aceec9e08b13ada94
Jon Breen <jonbreen@outlook.com>
2025-02-01 21:25:50 +0000

    Add podcast-name param

--- Console-Apps/CategorisePodcastEpisodes/CategorisePodcastEpisodesRequest.cs
+++ Console-Apps/CategorisePodcastEpisodes/CategorisePodcastEpisodesRequest.cs
@@ -5,8 +5,13 @@ namespace CategorisePodcastEpisodes;
 public class CategorisePodcastEpisodesRequest
 
 {
-    [Option('p', "podcast-ids", HelpText = "The Ids of the podcast to categorised, comma-separated")]
+    [Option('p', "podcast-ids", HelpText = "The Ids of the podcast to categorised, comma-separated",
+        Group = "Identifier")]
     public string? PodcastIds { get; set; }
+
+    [Option('n', "podcast-partial-match", HelpText = "The name, or part-name, of the podcast to enrich with images",
+        Group = "Identifier")]
+    public string PodcastPartialMatch { get; set; } = "";
 
     [Option('a', "recent", HelpText = "Categorise recently indexed episodes")]
     public bool CategoriseRecent { get; set; }
