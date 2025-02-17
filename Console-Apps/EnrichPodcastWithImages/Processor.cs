d119ee04d690a6e2b98a60673475064475393cf6
Jon Breen <jonbreen@outlook.com>
2025-02-17 15:35:44 +0000

    Add subject pption

---
+++ Console-Apps/EnrichPodcastWithImages/Processor.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.Logging;
+using RedditPodcastPoster.Models;
+using RedditPodcastPoster.Persistence.Abstractions;
+using RedditPodcastPoster.PodcastServices;
+using RedditPodcastPoster.PodcastServices.Abstractions;
+using RedditPodcastPoster.PodcastServices.Extensions;
+
+namespace EnrichPodcastWithImages;
+
+public class Processor(
+    IPodcastRepository podcastRepository,
+    IImageUpdater imageUpdater,
+    ILogger<Processor> logger)
+{
+    public async Task Run(Request request)
+    {
+        List<Guid> podcastIds;
+        Func<Episode, bool> episodeSelector;
+        if (!string.IsNullOrWhiteSpace(request.PodcastPartialMatch))
+        {
+            var ids =
+                await podcastRepository.GetAllBy(
+                        x => x.Name.ToLower().Contains(request.PodcastPartialMatch.ToLower()),
+                        p => new {id = p.Id})
+                    .ToListAsync();
+            podcastIds = ids.Select(x => x.id).ToList();
+            episodeSelector = episode => true;
+        }
+        else
+        {
+            var ids =
+                await podcastRepository.GetAllBy(
+                        x => x.Episodes.Any(x => x.Subjects.Contains(request.Subject)),
+                        p => new {id = p.Id})
+                    .ToListAsync();
+            podcastIds = ids.Select(x => x.id).ToList();
+            episodeSelector = episode => episode.Subjects.Contains(request.Subject);
+        }
+
+        if (!podcastIds.Any())
+        {
+            logger.LogError("No podcasts found for partial-name '{podcastPartialName}'.",
+                request.PodcastPartialMatch);
+            return;
+        }
+
+        var indexingContext = new IndexingContext();
+        foreach (var podcastId in podcastIds)
+        {
+            var updatedEpisodes = 0;
+
+            var podcast = await podcastRepository.GetBy(x => x.Id == podcastId);
+            if (podcast == null)
+            {
+                logger.LogError("No podcast with podcast-id '{podcastId}' found.", podcastId);
+                continue;
+            }
+
+            logger.LogInformation("Enriching podcast '{podcastName}'.", podcast.Name);
+            var episodes = podcast.Episodes.Where(episodeSelector);
+            foreach (var episode in episodes)
+            {
+                var imageUpdateRequest = (podcast, episode).ToEpisodeImageUpdateRequest();
+                var updated = await imageUpdater.UpdateImages(podcast, episode, imageUpdateRequest, indexingContext);
+                if (updated)
+                {
+                    updatedEpisodes++;
+                }
+            }
+
+            logger.LogInformation("Updated {updatedEpisodes} episodes.", updatedEpisodes);
+            if (updatedEpisodes > 0)
+            {
+                await podcastRepository.Save(podcast);
+            }
+        }
+    }
+}
--- Console-Apps/ThrowawayConsole/Program.cs
+++ Console-Apps/ThrowawayConsole/Program.cs
@@ -3,23 +3,15 @@ using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using RedditPodcastPoster.BBC;
-using RedditPodcastPoster.BBC.Extensions;
-using RedditPodcastPoster.Bluesky.Extensions;
 using RedditPodcastPoster.Cloudflare.Extensions;
-using RedditPodcastPoster.Common.Extensions;
 using RedditPodcastPoster.Configuration.Extensions;
-using RedditPodcastPoster.Persistence.Abstractions;
+using RedditPodcastPoster.ContentPublisher;
+using RedditPodcastPoster.ContentPublisher.Extensions;
+using RedditPodcastPoster.ContentPublisher.Models;
 using RedditPodcastPoster.Persistence.Extensions;
-using RedditPodcastPoster.PodcastServices.Abstractions;
-using RedditPodcastPoster.PodcastServices.Apple.Extensions;
-using RedditPodcastPoster.PodcastServices.Spotify.Extensions;
-using RedditPodcastPoster.PodcastServices.YouTube.Configuration;
-using RedditPodcastPoster.PodcastServices.YouTube.Extensions;
+using RedditPodcastPoster.Reddit.Extensions;
 using RedditPodcastPoster.Subjects.Extensions;
 using RedditPodcastPoster.Text.Extensions;
-using RedditPodcastPoster.UrlShortening.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -31,35 +23,23 @@ builder.Configuration
     .AddJsonFile("appsettings.json", false)
     .AddEnvironmentVariables("RedditPodcastPoster_")
     .AddCommandLine(args)
-    .AddSecrets(Assembly.GetExecutingAssembly());
+    .AddSecrets(Assembly.GetExecutingAssembly())
+    ;
 
 builder.Services
     .AddLogging()
-    .AddBBCServices()
     .AddRepositories()
-    .AddBlueskyServices()
-    .AddHttpClient()
-    .AddPostingCriteria()
+    .AddContentPublishing()
+    .AddCloudflareClients()
     .AddTextSanitiser()
     .AddSubjectServices()
-    .AddSpotifyServices()
-    .AddYouTubeServices(ApplicationUsage.Cli)
-    .AddCommonServices()
-    .AddShortnerServices()
-    .AddCloudflareClients()
-    .AddAppleServices()
-    .AddDelayedYouTubePublication();
+    .AddRedditServices();
 
 
 using var host = builder.Build();
 
-var repository= host.Services.GetService<IPodcastRepository>()!;
-var podcast = await repository.GetPodcast(Guid.Parse(args[0]));
-var component = host.Services.GetService<IAppleEpisodeRetrievalHandler>()!;
-var indexingContext = new IndexingContext(DateTime.Now.Subtract(TimeSpan.FromDays(2)));
-
-
-var (episodes, handled) = await component.GetEpisodes(podcast, indexingContext);
+var component = host.Services.GetService<IContentPublisher>()!;
+await component.PublishDiscoveryInfo(new DiscoveryInfo {DocumentCount = 0});
 
 return;
 
