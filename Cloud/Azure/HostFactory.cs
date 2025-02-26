fa8e82552f383884d51d0804f4ed96029e4a11ab
Jon Breen <jonbreen@outlook.com>
2025-02-26 19:00:36 +0000

    Use IHostApplicationBuilder

--- Cloud/Azure/HostFactory.cs
+++ Cloud/Azure/HostFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -7,34 +8,23 @@ namespace Azure;
 
 public static class HostFactory
 {
+
     public static IHost Create<T>(Action<IServiceCollection> configureServices) where T : class
     {
         var config = new ConfigurationBuilder().AddConfiguration<T>();
-        var host = new HostBuilder()
-            .ConfigureFunctionsWorkerDefaults()
-            .ConfigureAppConfiguration(builder =>
-            {
+        var builder = FunctionsApplication.CreateBuilder([]);
 #if DEBUG
-                builder.AddJsonFile("local.settings.json", false);
-                builder.AddConfiguration(config);
+        builder.Configuration.AddJsonFile("local.settings.json", false);
+        builder.Configuration.AddConfiguration(config);
 #endif
-            })
-            .ConfigureServices(services=>
-            {
-                services
-                    .AddLogging()
-                    //.AddApplicationInsightsTelemetryWorkerService()
-                    //.ConfigureFunctionsApplicationInsights()
-                    ;
-                configureServices(services);
-            })
-            .ConfigureLogging(logging =>
-            {
-                logging.ClearProviders();
-                //        logging.AllowAzureFunctionApplicationInsightsTraceLogging();
-            })
-            .Build();
-        return host;
-    }
+        builder.Services.AddLogging();
+        //builder.Services
+        //    .AddApplicationInsightsTelemetryWorkerService()
+        //    .ConfigureFunctionsApplicationInsights();
 
+        configureServices(builder.Services);
+        builder.Logging.ClearProviders();
+        //builder.Logging.AllowAzureFunctionApplicationInsightsTraceLogging();
+        return builder.Build();
+    }
 }
