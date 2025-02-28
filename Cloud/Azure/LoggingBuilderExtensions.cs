55ab602aa719ed549d9a9b6a4f7789d4c885346e
Jon Breen <jonbreen@outlook.com>
2025-02-28 17:42:50 +0000

    Clean up usings

--- Cloud/Azure/LoggingBuilderExtensions.cs
+++ Cloud/Azure/LoggingBuilderExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.ApplicationInsights;
-using Microsoft.VisualBasic;
 
 namespace Azure;
 
