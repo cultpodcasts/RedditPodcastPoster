313a958e44e7a90c36de43c5ae6cbc2bd77b001c
Jon Breen <jonbreen@outlook.com>
2025-03-01 14:09:56 +0000

    Remove set of basepath

--- Cloud/Azure/HostFactory.cs
+++ Cloud/Azure/HostFactory.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -12,9 +11,6 @@ public static class HostFactory
     public static IHost Create(string[] args, Action<IServiceCollection> configureServices)
     {
         var builder = FunctionsApplication.CreateBuilder(args);
-#if DEBUG
-        builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
-#endif
         var useAppInsights = builder.Configuration.UseApplicationInsightsConfiguration();
         Action<ILoggingBuilder> loggingBuilderAction =
             useAppInsights ? x => x.SetDefaultApplicationInsightsWarningRule() : x => { };
