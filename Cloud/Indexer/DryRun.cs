b3ea17f8e1c99b305167efea77fefec43c5330aa
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2025-09-08 11:57:02 +0100

    Disable social-posting (#728)

--- Cloud/Indexer/DryRun.cs
+++ Cloud/Indexer/DryRun.cs
@@ -4,8 +4,8 @@ internal static class DryRun
 {
     public static bool IsIndexDryRun = false;
     public static bool IsCategoriserDryRun = false;
-    public static bool IsPosterDryRun = false;
+    public static bool IsPosterDryRun = true;
     public static bool IsPublisherDryRun = false;
-    public static bool IsTweetDryRun = false;
-    public static bool IsBlueskyDryRun = false;
+    public static bool IsTweetDryRun = true;
+    public static bool IsBlueskyDryRun = true;
 }
