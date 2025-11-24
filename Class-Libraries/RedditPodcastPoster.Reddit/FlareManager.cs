77cc73e77979efbaee032abf78ea496faa446c59
MSI\jonbr <cultpodcasts@educocult.com>
2025-11-24 22:11:22 +0000

    Store final changes for step 'Upgrade Class-Libraries\RedditPodcastPoster.Reddit\RedditPodcastPoster.Reddit.csproj'

--- Class-Libraries/RedditPodcastPoster.Reddit/FlareManager.cs
+++ Class-Libraries/RedditPodcastPoster.Reddit/FlareManager.cs
@@ -15,7 +15,7 @@ public class FlareManager(
         var flareState = FlareState.Unknown;
         if (subjectNames.Any())
         {
-            var subjects = await subjectRepository.GetByNames(subjectNames).ToArrayAsync();
+            var subjects = await System.Linq.AsyncEnumerable.ToArrayAsync(subjectRepository.GetByNames(subjectNames), System.Threading.CancellationToken.None);
             var redditSubjects = subjects.Where(x => x.RedditFlairTemplateId != null);
             var subject =
                 redditSubjects.FirstOrDefault(x => x.SubjectType is null or SubjectType.Canonical) ??
