beb0ec958892a4006c1548e578c79c14eb5ea9f5
MSI\jonbr <cultpodcasts@educocult.com>
2025-12-09 15:19:02 +0000

    Improve usage of add-subject cli

--- Console-Apps/AddSubjectToSearchMatches/Processor.cs
+++ Console-Apps/AddSubjectToSearchMatches/Processor.cs
@@ -30,7 +30,8 @@ public class Processor(
         options.Select.Add("episodeDescription");
         options.Select.Add("subjects");
         options.Select.Add("podcastName");
-        var results = await searchClient.SearchAsync<SearchDocument>(request.Query, options);
+        var searchQuery = request.NotWholeTerm ? request.Query : $"\"{request.Query}\"";
+        var results = await searchClient.SearchAsync<SearchDocument>(searchQuery, options);
         if (results == null)
         {
             throw new InvalidOperationException("Results are null");
@@ -42,6 +43,15 @@ public class Processor(
         {
             throw new InvalidOperationException("All Search Results is null");
         }
+
+        var message = $"Episodes matching query: {allSearchResults.Count}, throttled at {request.Throttle}.";
+        if (allSearchResults.Count > request.Throttle)
+        {
+            logger.LogError(message);
+            return;
+        }
+
+        logger.LogInformation(message);
 
         var allSearchResultEpisodes = allSearchResults.Select(x =>
             new PodcastEpisode(x.Document.PodcastName!, x.Document.ToEpisodeModel()));
@@ -70,14 +80,20 @@ public class Processor(
                             podcastEpisode.Episode,
                             subjectEnrichmentOptions
                         );
-                        if (subjectMatches.Any(x => x.Subject.Name == request.Query) &&
-                            !repoPodcastEpisode.Subjects.Contains(request.Query))
+                        if (request.AddSubjectWhenNotSubjectMatch ||
+                            (subjectMatches.Any(x => x.Subject.Name == request.Query) &&
+                             !repoPodcastEpisode.Subjects.Contains(request.Query)))
                         {
                             updatedEpisodeIds.Add(repoPodcastEpisode.Id);
                             podcastChanged = true;
                             repoPodcastEpisode.Subjects.Add(request.Query);
                             logger.LogWarning(
                                 $"Podcast '{podcastName}' episode '{repoPodcastEpisode.Id}' has subject added.");
+                        }
+                        else
+                        {
+                            logger.LogWarning(
+                                $"Ignore podcast '{podcastName}' episode '{repoPodcastEpisode.Id}' - subject not added as not a subject match.");
                         }
                     }
                     else if (repoPodcastEpisode != null &&
--- Console-Apps/AddSubjectToSearchMatches/Request.cs
+++ Console-Apps/AddSubjectToSearchMatches/Request.cs
@@ -6,4 +6,13 @@ public class Request
 {
     [Value(0, MetaName = "query", HelpText = "Query to perform", Required = true)]
     public required string Query { get; set; }
+
+    [Value(1, MetaName = "throttle", HelpText = "Max permitted episodes to update", Default = 5)]
+    public required int Throttle { get; set; }
+
+    [Option('s', "add-subject-when-not-subject-match", Default = false, HelpText = "Bypass subject-matching on episode; add subject if is a search-result.")]
+    public bool AddSubjectWhenNotSubjectMatch { get; set; }
+
+    [Option('n', "not-whole-term", Default = false, HelpText = "Do not treat query as a quoted term.")]
+    public bool NotWholeTerm { get; set; }
 }
