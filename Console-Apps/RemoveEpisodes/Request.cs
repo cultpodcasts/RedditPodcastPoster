8768fee7f81a88e7944d68f5b1a0f77050796807
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2025-12-09 19:21:08 +0000

    Feature/improve remove episodes (#778)
    * Commit pending changes before .NET upgrade
    
    * Improve usage of add-subject cli
    
    * Added dry-run flag
    
    * Clean up
    
    * Add throttle, dry-run and whole-term-search to remove-episodes

--- Console-Apps/RemoveEpisodes/Request.cs
+++ Console-Apps/RemoveEpisodes/Request.cs
@@ -6,4 +6,13 @@ public class Request
 {
     [Value(0, MetaName = "query", HelpText = "Query to perform", Required = true)]
     public required string Query { get; set; }
+
+    [Value(1, MetaName = "throttle", HelpText = "Max permitted episodes to remove", Default = 5)]
+    public required int Throttle { get; set; }
+
+    [Option('n', "not-whole-term", Default = false, HelpText = "Do not treat query as a quoted term.")]
+    public bool NotWholeTerm { get; set; }
+
+    [Option('d', "dry-run", Default = false, HelpText = "Do not persist changes to database.")]
+    public bool IsDryRun { get; set; }
 }
