92f1ea98e039ec86c9823494386603aa267a4bcf
MSI\jonbr <cultpodcasts@educocult.com>
2025-12-19 11:44:23 +0000

    Disable reddit posting

--- Cloud/Indexer/DryRun.cs
+++ Cloud/Indexer/DryRun.cs
@@ -4,7 +4,7 @@ internal static class DryRun
 {
     public const bool IsIndexDryRun = false;
     public const bool IsCategoriserDryRun = false;
-    public const bool IsPosterDryRun = false;
+    public const bool IsPosterDryRun = true;
     public const bool IsPublisherDryRun = false;
     public const bool IsTweetDryRun = false;
     public const bool IsBlueskyDryRun = false;
