f0aa2183daa0d75ecd5e1ddbabf936bc6866a4bd
MSI\jonbr <cultpodcasts@educocult.com>
2025-12-19 13:05:50 +0000

    Remove literals

--- Cloud/Indexer/ActivityOptionsProvider.cs
+++ Cloud/Indexer/ActivityOptionsProvider.cs
@@ -8,37 +8,43 @@ public class ActivityOptionsProvider(IOptions<ActivityOptions> taskOptions) : IA
 
     public bool RunIndex(out string reason)
     {
-        reason = $"IsIndexDryRun= {DryRun.IsIndexDryRun}. RunIndex= {TaskOptions.RunIndex}";
+        reason =
+            $"{nameof(DryRun.IsIndexDryRun)}= {DryRun.IsIndexDryRun}. {nameof(TaskOptions.RunIndex)}= {TaskOptions.RunIndex}";
         return !DryRun.IsIndexDryRun && TaskOptions.RunIndex;
     }
 
     public bool RunCategoriser(out string reason)
     {
-        reason = $"IsCategoriserDryRun= {DryRun.IsCategoriserDryRun}. RunCategoriser= {TaskOptions.RunCategoriser}";
+        reason =
+            $"{nameof(DryRun.IsCategoriserDryRun)}= {DryRun.IsCategoriserDryRun}. {nameof(TaskOptions.RunCategoriser)}= {TaskOptions.RunCategoriser}";
         return !DryRun.IsCategoriserDryRun && TaskOptions.RunCategoriser;
     }
 
     public bool RunPoster(out string reason)
     {
-        reason = $"IsPosterDryRun= {DryRun.IsPosterDryRun}. RunPoster= {TaskOptions.RunPoster}";
+        reason =
+            $"{nameof(DryRun.IsPosterDryRun)}= {DryRun.IsPosterDryRun}. {nameof(TaskOptions.RunPoster)}= {TaskOptions.RunPoster}";
         return !DryRun.IsPosterDryRun && TaskOptions.RunPoster;
     }
 
     public bool RunPublisher(out string reason)
     {
-        reason = $"IsPublisherDryRun= {DryRun.IsPublisherDryRun}. RunPublisher= {TaskOptions.RunPublisher}";
+        reason =
+            $"{nameof(DryRun.IsPublisherDryRun)}= {DryRun.IsPublisherDryRun}. {nameof(TaskOptions.RunPublisher)}= {TaskOptions.RunPublisher}";
         return !DryRun.IsPublisherDryRun && TaskOptions.RunPublisher;
     }
 
     public bool RunTweet(out string reason)
     {
-        reason = $"IsTweetDryRun= {DryRun.IsTweetDryRun}. RunTweet= {TaskOptions.RunTweet}";
+        reason =
+            $"{nameof(DryRun.IsTweetDryRun)}= {DryRun.IsTweetDryRun}. {nameof(TaskOptions.RunTweet)}= {TaskOptions.RunTweet}";
         return !DryRun.IsTweetDryRun && TaskOptions.RunTweet;
     }
 
     public bool RunBluesky(out string reason)
     {
-        reason = $"IsBlueskyDryRun= {DryRun.IsBlueskyDryRun}. RunBluesky= {TaskOptions.RunBluesky}";
+        reason =
+            $"{nameof(DryRun.IsBlueskyDryRun)}= {DryRun.IsBlueskyDryRun}. {nameof(TaskOptions.RunBluesky)}= {TaskOptions.RunBluesky}";
         return !DryRun.IsBlueskyDryRun && TaskOptions.RunBluesky;
     }
 }
