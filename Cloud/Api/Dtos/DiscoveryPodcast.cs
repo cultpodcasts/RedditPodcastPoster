c384917b71b33ad3c841ac6ca43c46313c7f1a7f
MSI\jonbr <cultpodcasts@educocult.com>
2026-02-28 22:33:31 +0000

    Enhance podcast metadata in discovery API responses
    Refactor MatchingPodcasts to use new DiscoveryPodcast DTO, providing name, visibility, and visible episode count. Update related services and extensions to support richer podcast data in API responses.

---
+++ Cloud/Api/Dtos/DiscoveryPodcast.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Api.Dtos;
+
+public class DiscoveryPodcast
+{
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("visible")]
+    public required bool IsVisible { get; set; }
+
+    [JsonPropertyName("visibleEpisodes")]
+    public required int VisibleEpisodes { get; set; }
+}
