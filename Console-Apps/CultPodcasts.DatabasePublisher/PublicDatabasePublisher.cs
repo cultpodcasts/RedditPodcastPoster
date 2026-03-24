1c15c33ecf45b650dd71881f243b78021642ecc5
MSI\jonbr <cultpodcasts@educocult.com>
2026-03-24 11:28:31 +0000

    Remove EnrichSubjectRedditFlairs and Sqllite3DatabasePublisher
    Deleted both console apps and their source files from the repo, including project files and solution references. Also updated using statements in several files to include RedditPodcastPoster.Persistence.Legacy, with no functional changes.

--- Console-Apps/CultPodcasts.DatabasePublisher/PublicDatabasePublisher.cs
+++ Console-Apps/CultPodcasts.DatabasePublisher/PublicDatabasePublisher.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using RedditPodcastPoster.Models;
 using RedditPodcastPoster.Persistence;
 using RedditPodcastPoster.Persistence.Abstractions;
+using RedditPodcastPoster.Persistence.Legacy;
 
 namespace CultPodcasts.DatabasePublisher;
 
--- Console-Apps/Sqllite3DatabasePublisher/Request.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommandLine;
-
-namespace Sqllite3DatabasePublisher;
-
-public class Request
-{
-    [Value(0, HelpText = "Database Name", Required = true)]
-    public required string DatabaseName { get; set; }
-}
