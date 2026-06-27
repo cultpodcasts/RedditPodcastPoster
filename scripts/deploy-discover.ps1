2783b1239976a431847290c73497baea033ceb3a
MSI\jonbr <cultpodcasts@educocult.com>
2026-06-27 21:51:22 +0100

    Fix spacing

--- scripts/deploy-discover.ps1
+++ scripts/deploy-discover.ps1
@@ -1,76 +1,37 @@
 [CmdletBinding(SupportsShouldProcess = $true)]
-
 param(
-
     [string]$ResourceGroup,
-
     [string]$Suffix = 'infra',
-
     [string]$AppName,
-
     [string]$Runtime = 'linux-x64',
-
     [string]$Configuration = 'Release',
-
     [string]$StorageAccount,
-
     [string]$DeploymentContainer,
-
     [string]$DeploymentBlobName = 'released-package.zip',
-
     [ValidateSet('FlexBlob', 'FunctionAppDeploy')]
-
     [string]$DeploymentMode = 'FlexBlob',
-
     [switch]$NoRestore,
-
     [switch]$SkipPackaging,
-
     [switch]$RemoveUnsupportedRunFromPackageSetting
-
 )
 
-
-
 . (Join-Path $PSScriptRoot 'Resolve-DeploySettings.ps1')
-
-
-
 $jsonPath = Join-Path $PSScriptRoot 'deploy-discover.json'
-
 $settings = Resolve-DeploySettings -JsonPath $jsonPath -AppLabel 'Discover' -BoundParameters $PSBoundParameters
 
-
-
 $deployParams = @{
-
     FunctionName = 'discover'
-
     ResourceGroup = $settings.ResourceGroup
-
     AppName = $settings.AppName
-
     StorageAccount = $settings.StorageAccount
-
     DeploymentContainer = $settings.DeploymentContainer
-
 }
-
-
 
 foreach ($key in $PSBoundParameters.Keys) {
-
     if ($key -notin @('ResourceGroup', 'AppName', 'StorageAccount', 'DeploymentContainer')) {
-
         $deployParams[$key] = $PSBoundParameters[$key]
-
     }
-
 }
 
-
-
 & (Join-Path $PSScriptRoot 'deploy-function-local.ps1') @deployParams
-
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
-
