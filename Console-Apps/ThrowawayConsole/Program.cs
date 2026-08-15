e43c2f228138b2547a08133d66e3fc24b9cb5d17
MSI\jonbr <cultpodcasts@educocult.com>
2026-08-15 22:49:43 +0100

    Add version to clis

--- Console-Apps/ThrowawayConsole/Program.cs
+++ Console-Apps/ThrowawayConsole/Program.cs
@@ -3,20 +3,22 @@ using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RedditPodcastPoster.Configuration;
 using RedditPodcastPoster.Configuration.Extensions;
 using RedditPodcastPoster.Episodes.Extensions;
-using RedditPodcastPoster.InternetArchive.Extensions;
 using RedditPodcastPoster.Persistence.Extensions;
-using RedditPodcastPoster.PodcastServices.Abstractions;
 using RedditPodcastPoster.PodcastServices.Abstractions.Models;
-using RedditPodcastPoster.PodcastServices.YouTube.ChannelVideos;
 using RedditPodcastPoster.PodcastServices.YouTube.Clients;
 using RedditPodcastPoster.PodcastServices.YouTube.Configuration;
-using RedditPodcastPoster.PodcastServices.YouTube.Episode;
 using RedditPodcastPoster.PodcastServices.YouTube.Extensions;
-using RedditPodcastPoster.PodcastServices.YouTube.Services;
 using RedditPodcastPoster.PodcastServices.YouTube.Thumbnails;
 using RedditPodcastPoster.PodcastServices.YouTube.Video;
+
+if (args.Contains("--version"))
+{
+    VersionInfo.PrintVersion();
+    return 0;
+}
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -38,7 +40,7 @@ builder.Services
 
 using var host = builder.Build();
 
-if (args.Length == 0 )
+if (args.Length == 0)
 {
     Console.Error.WriteLine("Usage: ThrowawayConsole <internet-archive-url>");
     return 1;
@@ -48,8 +50,8 @@ var service = host.Services.GetRequiredService<IYouTubeServiceWrapper>();
 var service2 = host.Services.GetRequiredService<IYouTubeVideoService>();
 var service3 = host.Services.GetRequiredService<IYouTubeThumbnailResolver>();
 
-var video = await service2.GetVideoContentDetails(service, [args[0]], new IndexingContext(), withSnippets: true);
-var image= await service3.GetImageUrlAsync(video?.SingleOrDefault());
+var video = await service2.GetVideoContentDetails(service, [args[0]], new IndexingContext(), true);
+var image = await service3.GetImageUrlAsync(video?.SingleOrDefault());
 
 
 return 0;
